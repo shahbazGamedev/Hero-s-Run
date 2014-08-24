1bd96078c963997c324ae158217d271eac45b786
korbain <regis.g@videotron.ca>
2014-08-24 14:54:39 -0400

    Cemetery gate now opens by itself with a creaky sound

---
+++ Assets/Scripts/Level/CemeteryGateController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CemeteryGateController : MonoBehaviour {
+
+	void Start ()
+	{
+		Invoke( "openCemeteryGate", 1.5f );
+	}
+	
+	void openCemeteryGate()
+	{
+		Animation animation = GetComponent<Animation>();
+		audio.Play ();
+		animation.Play("open slowly");
+		animation.PlayQueued("sway in wind");
+	}
+
+}
