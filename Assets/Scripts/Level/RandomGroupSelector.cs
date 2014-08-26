3c044c8c79665da6af6200fc2f58e1335e07159d
korbain <regis.g@videotron.ca>
2014-08-26 13:02:55 -0400

    Added random group selector to Right tile

---
+++ Assets/Scripts/Level/RandomGroupSelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomGroupSelector : MonoBehaviour {
+
+	public GameObject group1;
+	public GameObject group2;
+	public float probabilityGroup1 = 0.5f;
+
+	void OnEnable ()
+	{
+		if( Random.value < probabilityGroup1 )
+		{
+			group1.SetActive( true );
+			group2.SetActive( false );
+		}
+		else
+		{
+			group1.SetActive( false );
+			group2.SetActive( true );
+		}
+	}
+}
