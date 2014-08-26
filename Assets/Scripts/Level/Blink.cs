45b2c864900bb39be885cca7815c332c49ed814c
korbain <regis.g@videotron.ca>
2014-08-25 21:37:33 -0400

    Added code to blink bat eyes

---
+++ Assets/Scripts/Level/Blink.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blink : MonoBehaviour {
+
+	public Texture eyesOpened;
+	public Texture eyesClosed;
+	bool eyesAreOpened = true;
+
+	// Use this for initialization
+	void Start () {
+	
+		Invoke("blink", 1f );
+	}
+	
+	void blink ()
+	{
+		if( eyesAreOpened )
+		{
+			renderer.material.mainTexture = eyesClosed;
+		}
+		else
+		{
+			renderer.material.mainTexture = eyesOpened;
+		}
+		eyesAreOpened = !eyesAreOpened;
+		Invoke("blink", Random.Range(0.4f, 1.5f) );
+
+	}
+}
