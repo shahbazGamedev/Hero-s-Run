76fc71d87ff09f913e57230cf43e4862a004b5f4
korbain <regis.g@videotron.ca>
2014-09-27 12:59:52 -0400

    1) fairy 20 cm higher 2) daisy on top hap now unlit 3) added 250 coin on coach 4) started 3d world map

---
+++ Assets/HandleSphere.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleSphere : MonoBehaviour {
+
+	float rotationIncrement = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		#if UNITY_EDITOR
+		handleKeyboard();
+		#endif
+
+	}
+
+	private void handleKeyboard()
+	{
+		//Also support keys for debugging
+		if ( Input.GetKeyDown (KeyCode.LeftArrow) ) 
+		{
+			transform.Rotate( Vector3.right, -rotationIncrement );
+		}
+		else if ( Input.GetKeyDown (KeyCode.RightArrow) ) 
+		{
+			transform.Rotate( Vector3.right, rotationIncrement );
+		}
+		else if ( Input.GetKeyDown (KeyCode.DownArrow) ) 
+		{
+			transform.Rotate( Vector3.up, rotationIncrement );
+		}
+		else if ( Input.GetKeyDown (KeyCode.UpArrow) ) 
+		{
+			transform.Rotate( Vector3.up, -rotationIncrement );
+		}
+	}
+
+	void rotateWorld( Vector3 rotationAxis, float amount )
+	{
+		transform.Rotate( rotationAxis, amount );
+	}
+
+}
