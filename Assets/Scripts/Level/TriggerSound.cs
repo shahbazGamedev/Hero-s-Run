68c1760557e62d9a9ec8853a39faf75edd375e9d
korbain <regis.g@videotron.ca>
2014-10-13 15:30:23 -0400

    Cemetery
    1) added crow sound to river crossing
    2) changed cart collision in 2 tiles to capsule (with mesh, you can get
    stuck, with box, you can be projected under the floor)
    3) removed some animators on floors

--- Assets/Scripts/Level/TriggerSound.cs
+++ Assets/Scripts/Level/TriggerSound.cs
@@ -4,11 +4,24 @@ using System.Collections;
 public class TriggerSound : MonoBehaviour {
 
 	public bool loop = true;
+	public GameObject objectWithAudioSource;
+	public float percentageChanceEventTriggered = 1f;
 
 	void OnTriggerEnter(Collider other)
 	{
-		Debug.Log ("TriggerSound " + audio.clip.name );
-		audio.loop = loop;
-		audio.Play();
+		if( other.name == "Hero" && Random.value <= percentageChanceEventTriggered )
+		{
+			if( objectWithAudioSource != null )
+			{
+				Debug.Log ("TriggerSound " + objectWithAudioSource.audio.clip.name );
+				objectWithAudioSource.audio.Play();
+			}
+			else
+			{
+				Debug.Log ("TriggerSound " + audio.clip.name );
+				audio.loop = loop;
+				audio.Play();
+			}
+		}
 	}
 }
