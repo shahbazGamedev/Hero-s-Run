aa928ad97e3f6159693368ab9d84687904e42c07
korbain <regis.g@videotron.ca>
2016-04-26 18:17:10 +0100

    Treasure Key tutorial
    Fairy appears and tells tutorial message to player.

--- Assets/TreasureKeyHandler.cs
+++ Assets/TreasureKeyHandler.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class TreasureKeyHandler : MonoBehaviour {
 
+	public bool playKeyTutorial = false;
+
+	PlayerController playerController;
+	FairyController fairyController;
+
+	
 	void Start ()
 	{
 		considerActivatingTreasureKey ();
@@ -15,4 +21,48 @@ public class TreasureKeyHandler : MonoBehaviour {
 		go.gameObject.transform.parent = gameObject.transform;
 	}
 	
+	public void keyPickedUp()
+	{
+		Debug.Log("TreasureKeyHandler: Player picked up treasure key.");
+
+		if ( playKeyTutorial )
+		{
+			GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+			playerController = playerObject.GetComponent<PlayerController>();
+	
+			GameObject fairyObject = GameObject.FindGameObjectWithTag("Fairy");
+			fairyController = fairyObject.GetComponent<FairyController>();
+
+			displayFairyMessage();
+		}
+		else
+		{
+			GetComponent<AudioSource>().Play(); //Only play pickup sound when no fairy, because fairy has an "appear" sound
+		}
+	}
+
+	void displayFairyMessage()
+	{
+		//Call fairy
+		fairyController.Appear ( FairyEmotion.Happy );
+
+		playerController.allowRunSpeedToIncrease = false;
+		Invoke ("step1", 1f );
+	}
+
+	//Fairy tells something to player
+	void step1()
+	{
+		AchievementDisplay.activateDisplayFairy( LocalizationManager.Instance.getText("TUTORIAL_TREASURE_KEY_FAIRY"), 0.35f, 3f );
+		//Player looks at fairy
+		playerController.lookOverShoulder( 0.4f, 2.75f );
+		Invoke ("step2", 3.5f );
+	}
+	
+	//Make the fairy disappear
+	void step2()
+	{
+		fairyController.Disappear ();
+		playerController.allowRunSpeedToIncrease = true;
+	}
 }
