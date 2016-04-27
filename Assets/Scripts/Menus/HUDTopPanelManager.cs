eaa7d2f57b8747bbba85e8e340db3a89a3765aa6
korbain <regis.g@videotron.ca>
2016-04-27 19:36:10 +0100

    First attempt at top HUD panel

---
+++ Assets/Scripts/Menus/HUDTopPanelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUDTopPanelManager : MonoBehaviour {
+
+	[Header("HUD Top Panel")]
+	public Text numberOfKeysText;
+	public Text numberOfStarsText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}	
+}
