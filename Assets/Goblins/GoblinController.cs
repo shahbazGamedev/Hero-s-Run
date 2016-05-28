9b5d1d653a6b120e5cc3a5d5272789138c05513d
korbain <regis.g@videotron.ca>
2016-05-28 16:20:19 +0100

    Goblin ouch sound and short range attack

--- Assets/Goblins/GoblinController.cs
+++ Assets/Goblins/GoblinController.cs
@@ -5,34 +5,23 @@ using System.Collections.Generic;
 public class GoblinController : BaseClass {
 
 	public enum GoblinState {
-		Reserved = -1,
-		Available = 0,
 		Idle = 1,
-		Walking = 2,
-		Crawling = 3,
+		Running = 2,
+		Attacking = 3,
 		Dying = 4,
-		BurrowUp = 5,
-		StandUpFromBack = 6,
-		Victory = 7
+		Victory = 5
 	}
 
-	public enum GoblinMoveType {
-		Walking = 1,
-		Crawling = 2,
-		Any = 3, //Either walk or crawl
-	}
-
-	static Transform player;
+	Transform player;
 	PlayerController playerController;
-	public CharacterController controller;
-	public Vector3 forward;
+	CharacterController controller;
+	Vector3 forward;
 	float walkSpeed = 1.65f; //good value so feet don't slide
-	public GoblinState goblinState = GoblinState.Available;
+	GoblinState goblinState = GoblinState.Idle;
+	bool hasAttackedPlayer = false;
 	public bool applyGravity = true;
 
-	public List<string> walkTypes = new List<string>();
-	public AudioClip moanLow;
-	public AudioClip moanHigh;
+	public AudioClip ouch;
 	public AudioClip fallToGround;
 	public AudioClip win;
 
@@ -42,7 +31,7 @@ public class GoblinController : BaseClass {
 	//For stats and achievements
 	static EventCounter goblin_bowling = new EventCounter( GameCenterManager.ZombieBowling, 3, 3000 );
 
-	//If true, the zombie heads for the player (as opposed to staying in its lane).
+	//If true, the goblin heads for the player (as opposed to staying in its lane).
 	public bool followsPlayer = false;
 
 	//True if the CharacterController is allowed to move and false otherwise (because the game is paused for example).
@@ -52,16 +41,6 @@ public class GoblinController : BaseClass {
 	void Awake () {
 
 		controller = (CharacterController) GetComponent("CharacterController");
-		controller.enabled = false;
-		Transform zombiePrefab;
-		if( gameObject.name == "Zombie Boy" )
-		{
-			zombiePrefab = transform.FindChild("zombieBoy");
-		}
-		else
-		{
-			zombiePrefab = transform.FindChild("zombieGirl");
-		}
 		player = GameObject.FindGameObjectWithTag("Player").transform;
 		playerController = (PlayerController) player.gameObject.GetComponent(typeof(PlayerController));
 		randomizeLook ();
@@ -74,14 +53,24 @@ public class GoblinController : BaseClass {
 		if( Random.value < 0.3f ) clothItem2.SetActive( false );
 	}
 
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		moveGoblin();
+		if( !hasAttackedPlayer )
+		{
+			float distance = Vector3.Distance(player.position,transform.position);
+
+			float attackDistance = 0.64f * PlayerController.getPlayerSpeed();
+			if( distance < attackDistance )
+			{
+				triggerShortRangeAttack();
+			}
+		}
 	}
 
 	void moveGoblin()
 	{
-		if( allowMove && goblinState == GoblinState.Walking || goblinState == GoblinState.Crawling )
+		if( allowMove && goblinState == GoblinState.Running )
 		{
 			//0) Target the player but we only want the Y rotation
 			if( followsPlayer )
@@ -89,7 +78,7 @@ public class GoblinController : BaseClass {
 				transform.LookAt( player );
 				transform.rotation = Quaternion.Euler( 0, transform.eulerAngles.y, 0 );
 			}
-			//1) Get the direction of the zombie
+			//1) Get the direction of the goblin
 			forward = transform.TransformDirection(Vector3.forward);			
 			//2) Scale vector based on run speed
 			forward = forward * Time.deltaTime * walkSpeed;
@@ -97,7 +86,6 @@ public class GoblinController : BaseClass {
 			//3) Move the controller
 			controller.Move( forward );
 		}
-
 	}
 
 	public GoblinState getGoblinState()
@@ -124,7 +112,7 @@ public class GoblinController : BaseClass {
 
 	public void sideCollision()
 	{
-		GetComponent<AudioSource>().PlayOneShot( moanLow );
+		GetComponent<AudioSource>().PlayOneShot( ouch );
 		GetComponent<Animator>().Play("damage");
 	}
 
@@ -146,95 +134,30 @@ public class GoblinController : BaseClass {
 		Debug.Log("KNOCKBACK " + gameObject.name );
 
 	}
-
-/*
-
-	void moveZombie()
-	{
-		if( allowMove && zombieState == ZombieState.Walking || zombieState == ZombieState.Crawling )
-		{
-			//0) Target the player but we only want the Y rotation
-			if( followsPlayer )
-			{
-				transform.LookAt( player );
-				transform.rotation = Quaternion.Euler( 0, transform.eulerAngles.y, 0 );
-			}
-			//1) Get the direction of the zombie
-			forward = transform.TransformDirection(Vector3.forward);			
-			//2) Scale vector based on run speed
-			forward = forward * Time.deltaTime * walkSpeed;
-			if( applyGravity ) forward.y -= 16f * Time.deltaTime;
-			//3) Move the controller
-			controller.Move( forward );
-		}
-
-	}
-
-	string selectRandomIdle()
-	{
-		int rd = Random.Range(0,3);
-		if( rd == 0 ) return "idle_normal";
-		else if( rd == 1 ) return "idle_lookAround";
-		else if( rd == 2 ) return "idle_scratchHead";
-		else return "idle_normal";
-	}
- 
-	public void walk()
-	{
-		controller.enabled = true;
-		string walkType = selectRandomWalk( ZombieMoveType.Walking );
-		anim.Play(walkType);
-		setZombieState( ZombieController.ZombieState.Walking );
-	}
-
-
-	void OnEnable()
-	{
-		PlayerController.playerStateChanged += PlayerStateChange;
-		GameManager.gameStateEvent += GameStateChange;
-	}
-
-	void OnDisable()
-	{
-		PlayerController.playerStateChanged -= PlayerStateChange;
-		GameManager.gameStateEvent -= GameStateChange;
-	}
-
-	void PlayerStateChange( CharacterState newState )
-	{
-		if( newState == CharacterState.Dying )
-		{
-		}
-	}
-	
-	void GameStateChange( GameState newState )
-	{
-		if( newState == GameState.Paused )
-		{
-			anim.enabled = false;
-			allowMove = false;
-			controller.enabled = false;
-			
-		}
-		else if( newState == GameState.Normal )
-		{
-			anim.enabled = true;
-			allowMove = true;
-			controller.enabled = true;
-		}
-	}
 	
 	void OnControllerColliderHit(ControllerColliderHit hit)
 	{
 		if( PlayerController._characterState == CharacterState.Dying )
 		{
-			if( hit.collider.name.StartsWith("Zombie") )
+			if( hit.collider.name.StartsWith("Goblin") )
 			{
-				//If a zombie collides with another zombie while the player is dead, have him stop moving and play the victory sequence.
+				//If a goblin collides with another goblin while the player is dead, have him stop moving and play the victory sequence.
 				victory( false );
 			}
 		}
 	}
 
-*/
+	void triggerShortRangeAttack()
+	{
+		hasAttackedPlayer = true;
+		if( Random.value < 0.5f )
+		{
+			GetComponent<Animator>().Play("attack1");
+		}
+		else
+		{
+			GetComponent<Animator>().Play("attack2");
+		}
+	}
+
 }
