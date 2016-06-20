28a68ea42fcb2b12db18c077bcf3575641d2d50a
korbain <regis.g@videotron.ca>
2016-06-20 07:53:08 +0100

    First pass collapsing bridge

---
+++ Assets/Scripts/Level/CollapsingBridge.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollapsingBridge : MonoBehaviour {
+
+	public GameObject leftFarTile;
+	public GameObject middleFarTile;
+	public GameObject rightFarTile;
+
+	PlayerController playerController;
+	Transform player;
+	bool hasCollapsed = false;
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		playerController = player.gameObject.GetComponent<PlayerController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		collapse();
+	}
+
+	void collapse()
+	{
+		if( !hasCollapsed )
+		{
+			float distance = Vector3.Distance(player.position,middleFarTile.transform.position);
+			float collapseDistance;
+			collapseDistance = 1f * PlayerController.getPlayerSpeed();
+			if( distance < collapseDistance  )
+			{
+				hasCollapsed = true;
+				//leftFarTile.GetComponent<Rigidbody>().isKinematic = false;
+				middleFarTile.GetComponent<Rigidbody>().isKinematic = false;
+				print("Collapse");
+				middleFarTile.GetComponent<Rigidbody>().AddForce( new Vector3( 0, 70f, 0 ) );
+				middleFarTile.GetComponent<Rigidbody>().AddTorque( new Vector3( 100f, 20f, -200f ) );
+				Invoke( "nextTime", 0.1f);
+			}
+		}
+	}
+	void nextTime()
+	{
+			rightFarTile.GetComponent<Rigidbody>().isKinematic = false;
+			rightFarTile.GetComponent<Rigidbody>().AddForce( new Vector3( 0, 70f, 0 ) );
+			rightFarTile.GetComponent<Rigidbody>().AddTorque( new Vector3( -100f, -20f, 200f ) );
+	}
+}
