fc1a070f391e21c6561dd41f605710cf3a3fd5f0
korbain <regis.g@videotron.ca>
2016-06-25 07:07:38 +0100

    Magical Mirror

---
+++ Assets/Scripts/Level/MagicalMirror.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicalMirror : MonoBehaviour {
+
+	public float fadeInDuration = 5f;
+	public float fadeOutDuration = 5f;
+	public float remainFadedDuration = 5f;
+
+	Material magicalMirrorMaterial;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		magicalMirrorMaterial = GetComponent<MeshRenderer>().material;
+		magicalMirrorMaterial.color = new Color( magicalMirrorMaterial.color.r, magicalMirrorMaterial.color.g, magicalMirrorMaterial.color.b, 0f );
+	}
+	
+	void Update ()
+	{
+		if ( Input.GetKeyDown (KeyCode.F) ) 
+		{
+			print("fading");
+			fadeInFarScene();
+		}
+	}
+
+	void fadeInFarScene()
+	{
+		LeanTween.color( gameObject, new Color( magicalMirrorMaterial.color.r, magicalMirrorMaterial.color.g, magicalMirrorMaterial.color.b, 1f ), fadeInDuration ).setOnComplete(fadeOutFarScene).setOnCompleteParam(gameObject);
+	}
+
+	void fadeOutFarScene()
+	{
+		LeanTween.color( gameObject, new Color( magicalMirrorMaterial.color.r, magicalMirrorMaterial.color.g, magicalMirrorMaterial.color.b, 0f ), fadeOutDuration ).setDelay(remainFadedDuration);
+	}
+
+}
