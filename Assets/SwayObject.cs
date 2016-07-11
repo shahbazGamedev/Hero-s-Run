0648b717b232a4ced537ea6c6afc16aff0b745ce
korbain <regis.g@videotron.ca>
2016-07-11 20:26:03 +0100

    Added swaying cages to cave entrance

---
+++ Assets/SwayObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Sways an object using a sine curve along Z axis only.
+public class SwayObject : MonoBehaviour {
+
+	public float strength = 4f;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.rotation = Quaternion.Euler( 0f, 0f, Mathf.Sin( Time.time * strength) );
+	}
+}
