4db6dcaf74f6695893e2eaf1fa796b1f172a8e13
korbain <regis.g@videotron.ca>
2016-07-28 23:33:43 +0100

    Blizzard avalanche tile (not finished)
    deleted needed tiles also

--- Assets/Scripts/Level/PushRigidBodies.cs
+++ Assets/Scripts/Level/PushRigidBodies.cs
@@ -1,21 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//On trigger enter by the hero, adds a small force and torque to the game objects with a rigid body as well as play a sound.
+//Adds a small force and torque to the game objects with a rigid body as well as plays a sound.
+[RequireComponent (typeof (AudioSource))]
 public class PushRigidBodies : MonoBehaviour {
+
+	public enum ActivationType {
+
+		OnTriggerEnter = 0,
+		PlayerDistance = 1
+	}
+
+	public ActivationType activationtype = ActivationType.OnTriggerEnter;
+	public Vector3 forceToApply = new Vector3( 0, 20f, 120f );
+	public Vector3 torqueToApply = new Vector3( 30f, 40f, 30f );
+	public float distanceMultiplier = 1f;
+
+	Transform player;
+	bool avalancheOccurred = false;
+
+	void Awake ()
+	{
+		if( activationtype == ActivationType.PlayerDistance )
+		{
+			player = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if( other.name == "Hero")
 		{
-			Rigidbody[] rigidBodies = GetComponentsInChildren<Rigidbody>();
-			for( int i = 0; i < rigidBodies.Length; i++ )
-			{
-				rigidBodies[i].AddForce( 0, 20f, 120f );
-				rigidBodies[i].AddTorque( 30f, 40f, 30f );
-			}
-			GetComponent<AudioSource>().PlayDelayed( 0.3f );
+			startAvalanche();
 		}
+	}
+	
+	void Update ()
+	{
+		if( activationtype == ActivationType.PlayerDistance ) checkIfAvalanche();
+	}
+
+	void checkIfAvalanche()
+	{
+		if( !avalancheOccurred )
+		{
+			float distance = Vector3.Distance(player.position, transform.position);
+			float  collapseDistance = distanceMultiplier * PlayerController.getPlayerSpeed();
+			if( distance < collapseDistance  )
+			{
+				avalancheOccurred = true;
+				startAvalanche();
+			}
+		}
+	}
+
+	void startAvalanche()
+	{
+		Rigidbody[] rigidBodies = GetComponentsInChildren<Rigidbody>();
+		for( int i = 0; i < rigidBodies.Length; i++ )
+		{
+			rigidBodies[i].AddForce( forceToApply );
+			rigidBodies[i].AddTorque( torqueToApply );
+			Destroy( rigidBodies[i].gameObject, 10f );
+		}
+		GetComponent<AudioSource>().PlayDelayed( 0.3f );
 	}
 
 }
