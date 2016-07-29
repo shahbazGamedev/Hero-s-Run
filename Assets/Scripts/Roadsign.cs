21693520e109d84364e303f216c1f3166a05a87e
korbain <regis.g@videotron.ca>
2016-07-29 22:33:34 +0100

    Improvements on Straight_River_Crossing

--- Assets/Scripts/Roadsign.cs
+++ Assets/Scripts/Roadsign.cs
@@ -2,16 +2,34 @@
 using System.Collections;
 
 public class Roadsign : MonoBehaviour {
+	
+	public enum SignType {
+			ShowLevelName = 0,
+			NormalText = 1
+	}
+
+	public SignType signType = SignType.ShowLevelName;
+	public float percentageDisplayed = 0.4f;
+	public string signText;
 
 	void Start ()
 	{
 		//We don't want the roadsign to be displayed at every corner
 		float rd = Random.Range( 0f, 1f );
-		if( rd > 0.6f )
+		if( rd <= percentageDisplayed )
 		{
-			//We are active, so display the name of the next destination
+			//We are active
 			GameObject roadLabel = transform.Find("Road Label").gameObject;
-			roadLabel.GetComponent<TextMesh>().text = LevelManager.Instance.getNextLevelName();
+			if( signType == SignType.ShowLevelName )
+			{
+				//Display the level name
+				roadLabel.GetComponent<TextMesh>().text = LevelManager.Instance.getNextLevelName();
+			}
+			else if( signType == SignType.NormalText )
+			{
+				//Display the specified text
+				roadLabel.GetComponent<TextMesh>().text = LocalizationManager.Instance.getText(signText);
+			}
 		}
 		else
 		{
