2cab98ceca8ce04e7704b7efeb2bd666559f30f6
korbain <regis.g@videotron.ca>
2016-07-31 11:19:01 +0100

    Blizzard + shadows
    1) falling rock can now kill you (but not always)
    2) sunk the two stumbles
    3) removed skirt on straight double
    4) changed end height on straight slope (lower by -0.1)
    5) hero and heroine receive shadows but no longer casts shadows. I am
    keeping the projector for that

---
+++ Assets/Level/Creatures/Goblins/FallingRock.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingRock : MonoBehaviour {
+
+	PlayerController playerController;
+	PowerUpManager powerUpManager;
+
+	void Start()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		playerController = player.GetComponent<PlayerController>();
+		//For power ups
+		powerUpManager = GameObject.FindGameObjectWithTag("PowerUpManager").GetComponent<PowerUpManager>();
+	}
+
+
+	public void OnCollisionEnter(Collision collision)
+	{
+	    //Debug.Log("FallingRock-OnCollisionEnter with " + collision.gameObject.name + " " + collision.contacts[0].normal );
+		//Play collision sound
+		//GetComponent<AudioSource>().Play();
+		if( collision.gameObject.name == "Hero" && collision.contacts[0].normal.y <= 0f )
+		{
+	   		Debug.LogWarning("FallingRock-OnCollisionEnter with " + collision.gameObject.name + " " + collision.contacts[0].normal );
+			//Is the player protected by a Shield Power Up?
+			if( PowerUpManager.isThisPowerUpActive( PowerUpType.Shield ) )
+			{
+				//This Power Up only works one time, so deactivate it
+				powerUpManager.deactivatePowerUp( PowerUpType.Shield, false );
+			}
+			else
+			{
+				playerController.managePlayerDeath ( DeathType.Obstacle );
+			}
+		}
+  	}
+}
