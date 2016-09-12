2fb39c291364b0975424d16a1fd5d54128ceae5d
korbain <regis.g@videotron.ca>
2016-09-12 19:39:48 +0100

    Jungle and title screen
    1) new tile - Landmark_Graveyard_5
    2) Moved fern in Straight
    3) Added Landmark_Graveyard_5 to level
    4) Added commented out code in TowerTitleScreen that needs 5.4

--- Assets/TowerTitleScreen.cs
+++ Assets/TowerTitleScreen.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class TowerTitleScreen : MonoBehaviour {
 
@@ -13,7 +14,7 @@ public class TowerTitleScreen : MonoBehaviour {
 	{
 		fairyController = fairy.GetComponent<FairyController>();
 		lightning.controlLightning(GameEvent.Start_Lightning);
-		Invoke("Step1", 1.5f );
+		Invoke("Step1", 3f );
 	}
 
 	void Step1 ()
@@ -39,4 +40,23 @@ public class TowerTitleScreen : MonoBehaviour {
 		titleScreenHandler.play();
 	}
 
+	/* Seems to require 5.4
+	void OnEnable()
+     {
+      //Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+         SceneManager.sceneLoaded += OnLevelFinishedLoading;
+     }
+ 
+     void OnDisable()
+     {
+     //Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+         SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+     }
+ 
+     void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+     {
+         Debug.Log("Level Loaded");
+         Debug.Log(scene.name);
+         Debug.Log(mode);
+     }*/
 }
