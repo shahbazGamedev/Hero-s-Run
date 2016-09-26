7a4b4e3a8dde57cb21d2c0d1dce5095c4e6781e5
korbain <regis.g@videotron.ca>
2016-09-26 22:49:38 +0100

    Cave Start changes
    1) wait for tap to play
    2) fairy speaks and skull falls and makes noise

---
+++ Assets/Scripts/Level/CavesArrivalStartSequence.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CavesArrivalStartSequence : MonoBehaviour {
+
+	FairyController fairyController;
+	public Rigidbody skull;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		GameObject fairyObject = GameObject.FindGameObjectWithTag("Fairy");
+		fairyController = fairyObject.GetComponent<FairyController>();
+	}
+
+	void Start ()
+	{
+		Invoke("step1", 1.5f );
+	}
+	
+	void step1()
+	{
+		fairyController.speak( "VO_FA_DARK_QUEEN_SABOTAGED", 4f, false );
+		Invoke("step2", 5f );
+	}
+
+	void step2()
+	{
+		skull.isKinematic = false;
+		skull.AddForce( 0,10f,5f);
+		Invoke("showTapToPlay", 5f );
+	}
+
+	void showTapToPlay()
+	{
+		GameManager.Instance.setGameState( GameState.Menu );
+	}
+
+}
---
+++ Assets/Scripts/Level/SoundOnCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundOnCollision : MonoBehaviour {
+
+	public void OnCollisionEnter(Collision collision)
+	{
+		//Play collision sound
+		if( !GetComponent<AudioSource>().isPlaying ) GetComponent<AudioSource>().Play();
+	}
+}
