aca7eb5a0e78f6143ebf8ea4456e0f7c764d7061
korbain <regis.g@videotron.ca>
2016-09-28 19:14:33 +0100

    Caves Straight log tile

--- Assets/Scripts/Level/CavesArrivalStartSequence.cs
+++ Assets/Scripts/Level/CavesArrivalStartSequence.cs
@@ -28,7 +28,7 @@ public class CavesArrivalStartSequence : MonoBehaviour {
 	{
 		skull.isKinematic = false;
 		skull.AddForce( 0,10f,5f);
-		Invoke("showTapToPlay", 3.5f );
+		Invoke("showTapToPlay", 3f );
 	}
 
 	void showTapToPlay()
