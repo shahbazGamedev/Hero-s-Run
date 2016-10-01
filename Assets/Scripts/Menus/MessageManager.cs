3ddd869d5c1e721613b9fd7956a4ec1238a27842
korbain <regis.g@videotron.ca>
2016-10-01 11:12:21 +0100

    More work on message center
    1) handles no mail
    2) handles no Internet access
    3) handles Accept button

--- Assets/Scripts/Menus/MessageManager.cs
+++ Assets/Scripts/Menus/MessageManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MessageManager : MonoBehaviour {
 
@@ -20,16 +21,38 @@ public class MessageManager : MonoBehaviour {
 	[Header("Menu Prefabs")]
 	public RectTransform content;
 	public GameObject messageEntryPrefab;
+	public GameObject mailInformationPanel;
+	public Text mailInformationText;
 
-	// Use this for initialization
 	public void refreshMessages ()
 	{
-		for( int i = 0; i < FacebookManager.Instance.AppRequestDataList.Count; i++ )
+		//Player is connected to the Internet
+		if( Application.internetReachability != NetworkReachability.NotReachable )
 		{
-			GameObject go = (GameObject)Instantiate(messageEntryPrefab);
-			go.transform.SetParent(content.transform,false);
-			go.name = "Message number " + i.ToString();
-			go.GetComponent<MessageEntry>().initializeMessage( FacebookManager.Instance.AppRequestDataList[i] );
+			if( FacebookManager.Instance.AppRequestDataList.Count > 0 )
+			{
+				//Player has mail and is connected to the Internet.
+				mailInformationPanel.SetActive( false );
+				for( int i = 0; i < FacebookManager.Instance.AppRequestDataList.Count; i++ )
+				{
+					GameObject go = (GameObject)Instantiate(messageEntryPrefab);
+					go.transform.SetParent(content.transform,false);
+					go.name = "Message number " + i.ToString();
+					go.GetComponent<MessageEntry>().initializeMessage( FacebookManager.Instance.AppRequestDataList[i] );
+				}
+			}
+			else
+			{
+				//Player has no mail but is connected to the Internet.
+				mailInformationText.text = LocalizationManager.Instance.getText("POPUP_MESSAGE_CENTER_NO_MAIL");
+				mailInformationPanel.SetActive( true );
+			}
+		}
+		else
+		{
+			//Player is not connected to the Internet.
+			mailInformationText.text = LocalizationManager.Instance.getText("MENU_CONNECTION_FAILED_TEXT");
+			mailInformationPanel.SetActive( true );
 		}
 	}
 	
--- Assets/Scripts/Store/MessageEntry.cs
+++ Assets/Scripts/Store/MessageEntry.cs
@@ -1,16 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using Facebook.Unity;
 
 public class MessageEntry : MonoBehaviour {
 
 	[Header("Message Entry")]
 	public Image icon;
 	public Text message;
+	AppRequestData requestData;
 
-	public void initializeMessage( AppRequestData data )
+	public void initializeMessage( AppRequestData requestData )
 	{
-		message.text = data.dataType.ToString() + " " + data.fromFirstName;
-		icon.GetComponent<FacebookPortraitHandler>().setPortrait( data.fromID );
+		this.requestData = requestData;
+		message.text = requestData.dataType.ToString() + " " + requestData.fromFirstName;
+		icon.GetComponent<FacebookPortraitHandler>().setPortrait( requestData.fromID );
 	}
+
+	public void buttonPressed()
+	{
+		Debug.Log("MessageEntry-buttonPressed: " + requestData.dataType );
+		switch (requestData.dataType)
+		{
+			case RequestDataType.Ask_Give_Life:
+				FacebookManager.Instance.CallAppRequestAsDirectRequest("App Requests", LocalizationManager.Instance.getText("FB_HAVE_A_LIFE_MESSAGE"), requestData.fromID, "Accept_Give_Life," + requestData.dataNumber.ToString(), MCHCallback, requestData.appRequestID );
+				break;
+			case RequestDataType.Accept_Give_Life:
+				Debug.Log("MessageEntry-buttonPressed: Accept_Give_Life" );
+				PlayerStatsManager.Instance.increaseLives(1);
+				PlayerStatsManager.Instance.savePlayerStats();
+				//Now that it is successfully processed, delete the app request on Facebook
+				//FacebookManager.Instance.deleteAppRequest( appRequestData.appRequestID );
+				//Slide out the message entry and then remove it from the Content RectTransform
+				GameObject.Destroy( gameObject );
+				break;
+			case RequestDataType.Unknown:
+				Debug.LogWarning("MessageEntry-processNextAppRequest: unknown data type specified." );
+				break;
+		}
+
+	}
+
+	public void MCHCallback(IAppRequestResult result, string appRequestIDToDelete )
+	{
+		if (result.Error != null)
+		{
+			Debug.Log ("MessageEntry-MCHCallback error:\n" + result.Error );
+		}
+		else
+		{
+			Debug.Log ("MessageEntry-MCHCallback success:\n" + result.RawResult );
+			if( !result.RawResult.Contains("cancelled") )
+			{
+				//Now that it is successfully processed, delete the app request on Facebook
+				//FacebookManager.Instance.deleteAppRequest( appRequestIDToDelete );
+			}
+			else
+			{
+				Debug.Log ("MessageEntry-MCHCallback user cancelled." );
+			}
+		}
+	}
+
 }
