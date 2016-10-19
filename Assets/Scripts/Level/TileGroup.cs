e6b5dfa7da854addbc8d8fbfeaa3947b43c12faa
korbain <regis.g@videotron.ca>
2016-10-19 08:00:59 +0100

    TileGroup script

---
+++ Assets/Scripts/Level/TileGroup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum ValidGameMode {
+	Story = 1,
+	Endless = 2,
+	Any = 3
+}
+
+[System.Serializable]
+public class TileGroup{
+
+	[Header("Tile Group Parameters")]
+	[Tooltip("The theme.")]
+	public SegmentTheme theme = SegmentTheme.Forest;
+	public string name;
+	public string description;
+	public ValidGameMode ValidGameMode = ValidGameMode.Any;
+	//Other ideas: validDifficultyLevel, allowEnemies, requiresZeroRotation, hasZeroRotation
+	public List<TileType> tileList = new List<TileType>();
+	[Range(0,100)]
+	public int frequency = 0;
+}
