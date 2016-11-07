736b5dea456fcbc4a474f69da21fc94debca9c67
korbain <regis.g@videotron.ca>
2016-11-07 18:49:42 +0000

    Restart from checkpoint plus flag smaller

---
+++ Assets/Scripts/Menus/FadeInCanvasGroup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeInCanvasGroup : MonoBehaviour {
+	
+	public float fadeInDuration = 0.5f;
+
+	void OnEnable ()
+	{
+		LeanTween.alphaCanvas( GetComponent<CanvasGroup>(), 1f, fadeInDuration );		
+	}
+
+	void OnDisable ()
+	{
+		GetComponent<CanvasGroup>().alpha = 0;		
+	}
+	
+}
