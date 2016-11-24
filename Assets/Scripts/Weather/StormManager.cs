027af3e73fac4c1ace73063ef262ffc2ec7c86d3
korbain <regis.g@videotron.ca>
2016-11-24 21:12:34 +0000

    Storm haze changes
    1) lowered ambience a bit
    2) to avoid seeing the haze rise up vertically when the storm starts,
    the value is set to 350  right from the start and the skyAlpha is
    leaped instead

--- Assets/Scripts/Weather/StormManager.cs
+++ Assets/Scripts/Weather/StormManager.cs
@@ -71,7 +71,7 @@ public class StormManager : MonoBehaviour {
 		dynamicFog.alpha =  Mathf.Lerp( 0.6f, 0.96f, intensity );
 
 		//Sky Properties
-		dynamicFog.skyHaze = Mathf.Lerp( 26f, 350f, intensity );
+		dynamicFog.skyAlpha = Mathf.Lerp( 0f, 0.93f, intensity );
 
 		//Force the materials to update
 		dynamicFog.UpdateMaterialProperties();
@@ -110,23 +110,18 @@ public class StormManager : MonoBehaviour {
 		dynamicFog.noiseStrength = Mathf.Lerp( 0.4f, 0.45f, intensity );
 		dynamicFog.distance = Mathf.Lerp( 0.01f, 0f, intensity );
 		dynamicFog.distanceFallOff = Mathf.Lerp( 0.04f, 0.028f, intensity );
-		dynamicFog.maxDistance = Mathf.Lerp( 0.999f, 0.999f, intensity );
-		dynamicFog.maxDistanceFallOff = Mathf.Lerp(0f, 0f, intensity );
 		dynamicFog.height = Mathf.Lerp( 20f, 83f, intensity );
 		dynamicFog.heightFallOff = Mathf.Lerp( 1f, 0f, intensity );
-		dynamicFog.baselineHeight = Mathf.Lerp(0f, 0f, intensity );
 		dynamicFog.turbulence = Mathf.Lerp( 0.4f, 15f, intensity );
 		dynamicFog.speed = Mathf.Lerp( 0.005f, 0.2f, intensity );
 
 		//Sky Properties
-		dynamicFog.skyHaze = Mathf.Lerp( 350f, 337f, intensity );
 		dynamicFog.skySpeed = Mathf.Lerp( 0.3f, 0.49f, intensity );
 		dynamicFog.skyNoiseStrength = Mathf.Lerp( 0.6f, 0.72f, intensity );
 		dynamicFog.skyAlpha = Mathf.Lerp( 0.93f, 0.97f, intensity );
 
 		//Force the materials to update
 		dynamicFog.UpdateMaterialProperties();
-
 	}
 
 	void setFoliageMovementIntensity( float intensity )
