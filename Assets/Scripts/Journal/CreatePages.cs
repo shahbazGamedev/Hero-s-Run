528df73e12e9d25211d2235a753b54377ce37897
korbain <regis.g@videotron.ca>
2017-01-04 04:07:16 +0000

    Journal - fixed the case of the missing letters
    +because the number of visible characters changes depending on the
    letters forming the text.

--- Assets/Scripts/Journal/CreatePages.cs
+++ Assets/Scripts/Journal/CreatePages.cs
@@ -15,7 +15,6 @@ public class CreatePages : MonoBehaviour {
 	public List<string> pageTexts = new List<string>();
 	RenderTexture renderTexture;
 	bool levelLoading = false;
-	int numberOfCharactersPerPage = 272;
 	public List<Sprite> testCovers = new List<Sprite>();
 	string storyForTestPuposes = "Test! The demon materialized out of nowhere. When one of his two hoof touches the ground, a network of spidery cracks appears below, filled with flamelets. One of his black horns is broken, but the other is sharp as a spear. His eyes have a glint of evil. His sinister intent is clear. He is here for our treasure. How did he pass our protection spells, glyphs of protections and sigils? The demon laughed. He had appeared inside the Golden Vault. The treasure was within tantalizing reach. In the chest, a score feet away from him was a chest filled with enough fairy dust to resurrect an entire army. And my liege, King Merrylock, all dressed in purple and yellow, the most powerful mage of the Kingdom of Lum lied on a pile of shiny coins in a drunken stupor. It was up to me, Lily, to save the day. I was small, well tiny really, like all fairies. On a good day, I measured 1 foot. Okay, 11 inches to be precise if your counting. I had graduated from fairy school a full two weeks ago. Now graduating was a big event for me as I had failed my first year. And as all young graduates, I had been assigned to guard duty. Or like Silvestra said, to guard, the most precious treasure of the kingdom. It was boring, boring, boring. Nothing ever happened to it. Our liege, King Merrylock, was the most powerful mage of the Kingdom of Lum. The last person who tried to steal our treasure, one Balthazar More, had been transmogrified into a squiggly piglet.";
 
@@ -79,18 +78,15 @@ public class CreatePages : MonoBehaviour {
 			story = storyForTestPuposes;
 		#endif
  
-		//step 2 - establish the number of visible characters that can fit in pageText.
-		pageText.text = story;		
-		Canvas.ForceUpdateCanvases(); 	//This is needed or else the TextGenerator values will not have been updated
-		numberOfCharactersPerPage = pageText.cachedTextGenerator.characterCount;
-		pageText.text = string.Empty;		
-
-		//step 3 - populate pageTexts
+		//step 2 - populate pageTexts
 		pageTexts.Clear();
 		int pageCounter = 0;
+		int numberOfCharactersPerPage = 0;
 
 		while ( story.Length > 0 )
 		{
+			numberOfCharactersPerPage = getNumberOfVisibleCharacters( story );
+
 			if( story.Length > numberOfCharactersPerPage )
 			{
 				//if we are here, it means that what is left of the story does not fit in a single page.
@@ -110,7 +106,7 @@ public class CreatePages : MonoBehaviour {
 		}
 		book.setBookSize(pageCounter + 1 ); //plus one because of the cover
 
-		//Add book cover
+		//step 3 - add book cover
 		int randomCover = UnityEngine.Random.Range( 1, 8 );
 		#if UNITY_EDITOR
 		//For the time being, the asset bundles that store the covers,stories, and entries are on my Mac and not on the web.
@@ -143,6 +139,15 @@ public class CreatePages : MonoBehaviour {
 			if( Char.IsSeparator( storyChar[i] ) || Char.IsPunctuation( storyChar[i] ) ) return i;
 		}
 		return -1;
+	}
+
+	//Used to establish the number of visible characters that can fit in pageText. Remember that
+	//the size of a character is typically not constant in a font. A "1" is narrower than a "W" for example.
+	public int getNumberOfVisibleCharacters( string text )
+	{
+		pageText.text = text;		
+		Canvas.ForceUpdateCanvases(); 	//This is needed or else the TextGenerator values will not have been updated
+		return pageText.cachedTextGenerator.characterCount;
 	}
 
 	public void closeMenu()
