3a03c9e854c290d0230df466125cba247258be0a
korbain <regis.g@videotron.ca>
2017-01-06 19:58:31 +0000

    Journal JournalAssetManager now uses hashes
    This means that the game will always download the latest version :)

--- Assets/Scripts/Journal/JournalAssetManager.cs
+++ Assets/Scripts/Journal/JournalAssetManager.cs
@@ -8,10 +8,9 @@ public class JournalAssetManager : MonoBehaviour {
 
 	public static JournalAssetManager Instance;
 	[Tooltip("The URL format for testing locally on a Mac is: file:///Users/regisgeoffrion/Documents/workspace/AssetBundles/covers. You need the 3 slashes after file:")]
-    public string entriesBundleURL;
-    public string coversBundleURL;
-    public string storiesBundleURL;
-    public int version;
+    public string assetBundleFolder;
+    public string assetBundleManifest;
+	Dictionary<string, AssetBundle> assetBundleDictionary = new Dictionary<string, AssetBundle>();
 	public Dictionary<string, Sprite> covers = new Dictionary<string, Sprite>();
 	public Dictionary<string, TextAsset> stories = new Dictionary<string, TextAsset>();
 	public TextAsset entriesJson;
@@ -39,114 +38,134 @@ public class JournalAssetManager : MonoBehaviour {
 		GameManager.Instance.journalAssetManager = this;
 		DontDestroyOnLoad( this );
 		#if UNITY_EDITOR
-        StartCoroutine (DownloadAndCacheEntries());
-        StartCoroutine (DownloadAndCacheCovers());
-        StartCoroutine (DownloadAndCacheStories());
+		StartCoroutine ( getAssetBundleManifest() );
 		#endif
     }
 
-   	IEnumerator DownloadAndCacheEntries ()
+	IEnumerator getAssetBundleManifest()
 	{
 		// Wait for the Caching system to be ready
 		while (!Caching.ready)
             yield return null;
 
 		// Load the AssetBundle file from Cache if it exists with the same version or download and store it in the cache
-        using(WWW www = WWW.LoadFromCacheOrDownload (entriesBundleURL, version))
+		using(WWW wwwManifest = new WWW (assetBundleFolder + assetBundleManifest))
 		{
-            yield return www;
-            if (www.error != null)
+            yield return wwwManifest;
+            if (wwwManifest.error != null)
 			{
-                throw new Exception( "WWW download had an error:" + www.error );
-				yield break;
+                throw new Exception( "WWW download had an error:" + wwwManifest.error );
+				yield return null;
 			}
-            AssetBundle bundle = www.assetBundle;
-			//Load the journal manifest (entries)
-			AssetBundleRequest assetLoadAllTextRequest = bundle.LoadAllAssetsAsync<TextAsset>();
-			yield return assetLoadAllTextRequest;
-			UnityEngine.Object[] prefabs = assetLoadAllTextRequest.allAssets;
-	
-			entriesJson = Instantiate<TextAsset>(prefabs[0] as TextAsset );
-			Debug.Log("JournalAssetManager-Entries found: " + entriesJson.text );
+			AssetBundle manifestBundle = wwwManifest.assetBundle;
+			AssetBundleManifest manifest = manifestBundle.LoadAsset("AssetBundleManifest") as AssetBundleManifest;
+			manifestBundle.Unload( false );
 
-			// Unload the AssetBundles compressed contents to conserve memory
-			bundle.Unload(false);
+			//Get all asset bundles 
+			string[] allAssetBundles = manifest.GetAllAssetBundles();
+			Debug.Log("Manifest name: " + manifest.name + " has " + allAssetBundles.Length + " bundles including all variants." );
 
-			GameObject.FindObjectOfType<JournalManager>().updateEntries( entriesJson.text );
-
-        } // memory is freed from the web stream (www.Dispose() gets called implicitly)
+			//Load all asset bundles 
+			AssetBundle[] assetBundles = new AssetBundle[allAssetBundles.Length];
+			for( int i = 0; i < allAssetBundles.Length; i++ )
+			{
+				if( allAssetBundles[i].Contains(".") )
+				{
+					//This means it is a variant because the name terminates in .english, or .french, etc.
+					//Is this variant in a language we are interested in?
+					if( allAssetBundles[i].EndsWith( getLanguage() ) )
+					{
+						//Yes, this is the correct language. Add it to our dictionary.
+						//We don't want to load and cache assets that are not for our language for nothing.
+						string assetBundlePath = assetBundleFolder + allAssetBundles[i];
+						//Get the hash
+						Hash128 hash = manifest.GetAssetBundleHash( allAssetBundles[i] );
+						WWW www = WWW.LoadFromCacheOrDownload (assetBundlePath, hash);
+						yield return www;
+						if (www.error != null)
+						{
+							throw new Exception( "WWW download had an error:" + www.error + " for " +  assetBundlePath );
+							yield break;
+						}
+						assetBundleDictionary.Add( allAssetBundles[i], www.assetBundle );
+						Debug.Log("Adding asset bundle variant: " + allAssetBundles[i] + " to dictionary with hash " + hash.ToString() );
+					}
+				}
+				else
+				{
+					string assetBundlePath = assetBundleFolder + allAssetBundles[i];
+					//Get the hash
+					Hash128 hash = manifest.GetAssetBundleHash( allAssetBundles[i] );
+					WWW www = WWW.LoadFromCacheOrDownload (assetBundlePath, hash);
+					yield return www;
+					if (www.error != null)
+					{
+						throw new Exception( "WWW download had an error:" + www.error + " for " +  assetBundlePath );
+						yield break;
+					}
+					assetBundleDictionary.Add( allAssetBundles[i], www.assetBundle );
+					Debug.Log("Adding normal asset bundle: " + allAssetBundles[i] + " to dictionary with hash " + hash.ToString() );
+				}
+			}
+		}
+		StartCoroutine( createEntries("entries") );
+		StartCoroutine( createCovers("covers") );
+		StartCoroutine( createStories("stories" + "." + getLanguage()) );
 	}
 
-    IEnumerator DownloadAndCacheCovers ()
+	IEnumerator createEntries( string assetBundleName )
 	{
-        // Wait for the Caching system to be ready
-        while (!Caching.ready)
-            yield return null;
+		AssetBundle bundle = assetBundleDictionary[assetBundleName];
+		//Load the journal manifest (entries)
+		AssetBundleRequest assetLoadAllTextRequest = bundle.LoadAllAssetsAsync<TextAsset>();
+		yield return assetLoadAllTextRequest;
+		UnityEngine.Object[] prefabs = assetLoadAllTextRequest.allAssets;
 
-        //Load the AssetBundle file from Cache if it exists with the same version or download and store it in the cache
-		//Load story covers
-        using(WWW www = WWW.LoadFromCacheOrDownload (coversBundleURL, version))
-		{
-            yield return www;
-            if (www.error != null)
-			{
-                throw new Exception( "WWW download had an error:" + www.error );
-				yield break;
-			}
-            AssetBundle bundle = www.assetBundle;
+		entriesJson = Instantiate<TextAsset>(prefabs[0] as TextAsset );
+		Debug.Log("JournalAssetManager-Entries found: " + entriesJson.text );
 
-			//Load all cover sprites
-			AssetBundleRequest assetLoadAllSpriteRequest = bundle.LoadAllAssetsAsync<Sprite>();
-			yield return assetLoadAllSpriteRequest;
-			UnityEngine.Object[] prefabs = assetLoadAllSpriteRequest.allAssets;
-	
-			Debug.Log("JournalAssetManager-Number of covers found: " + prefabs.Length);
-			for( int i = 0; i < prefabs.Length; i++ )
-			{
-				covers.Add( prefabs[i].name, Instantiate<Sprite>(prefabs[i] as Sprite ) );
+		GameObject.FindObjectOfType<JournalManager>().updateEntries( entriesJson.text );
+		assetBundleDictionary[assetBundleName].Unload(false);
+		assetBundleDictionary[assetBundleName] = null;
+	}
 
-			}
-
-           // Unload the AssetBundles compressed contents to conserve memory
-           bundle.Unload(false);
-
-        } // memory is freed from the web stream (www.Dispose() gets called implicitly)
-
-    }
-
-    IEnumerator DownloadAndCacheStories ()
+	IEnumerator createCovers( string assetBundleName )
 	{
-        // Wait for the Caching system to be ready
-        while (!Caching.ready)
-            yield return null;
+		AssetBundle bundle = assetBundleDictionary[assetBundleName];
+		//Load all cover sprites
+		AssetBundleRequest assetLoadAllSpriteRequest = bundle.LoadAllAssetsAsync<Sprite>();
+		yield return assetLoadAllSpriteRequest;
+		UnityEngine.Object[] prefabs = assetLoadAllSpriteRequest.allAssets;
 
-		// Load the AssetBundle file from Cache if it exists with the same version or download and store it in the cache
-        using(WWW www = WWW.LoadFromCacheOrDownload (storiesBundleURL + "." + getLanguage(), version))
+		Debug.Log("JournalAssetManager-Number of covers found: " + prefabs.Length);
+		for( int i = 0; i < prefabs.Length; i++ )
 		{
-            yield return www;
-            if (www.error != null)
-			{
-                throw new Exception( "WWW download had an error:" + www.error );
-				yield break;
-			}
-            AssetBundle bundle = www.assetBundle;
-			//Load all story texts
-			AssetBundleRequest assetLoadAllTextRequest = bundle.LoadAllAssetsAsync<TextAsset>();
-			yield return assetLoadAllTextRequest;
-			UnityEngine.Object[] prefabs = assetLoadAllTextRequest.allAssets;
-	
-			Debug.Log("JournalAssetManager-Number of texts found:  " + prefabs.Length);
-			for( int i = 0; i < prefabs.Length; i++ )
-			{
-				stories.Add( prefabs[i].name, Instantiate<TextAsset>(prefabs[i] as TextAsset ) );
+			covers.Add( prefabs[i].name, Instantiate<Sprite>(prefabs[i] as Sprite ) );
 
-			}
+		}
 
-           // Unload the AssetBundles compressed contents to conserve memory
-           bundle.Unload(false);
+		assetBundleDictionary[assetBundleName].Unload(false);
+		assetBundleDictionary[assetBundleName] = null;
+	}
 
-        } // memory is freed from the web stream (www.Dispose() gets called implicitly)
-    }
+ 	IEnumerator createStories( string assetBundleName )
+	{
+		AssetBundle bundle = assetBundleDictionary[assetBundleName];
+		//Load all story texts
+		AssetBundleRequest assetLoadAllTextRequest = bundle.LoadAllAssetsAsync<TextAsset>();
+		yield return assetLoadAllTextRequest;
+		UnityEngine.Object[] prefabs = assetLoadAllTextRequest.allAssets;
+
+		Debug.Log("JournalAssetManager-Number of texts found:  " + prefabs.Length);
+		for( int i = 0; i < prefabs.Length; i++ )
+		{
+			stories.Add( prefabs[i].name, Instantiate<TextAsset>(prefabs[i] as TextAsset ) );
+
+		}
+
+		assetBundleDictionary[assetBundleName].Unload(false);
+		assetBundleDictionary[assetBundleName] = null;
+	}
 
    	string getLanguage()
 	{
