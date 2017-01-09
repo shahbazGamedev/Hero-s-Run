63836af33630ad4edd16232e4779d01582ca7e42
korbain <regis.g@videotron.ca>
2017-01-09 18:14:54 +0000

    Fixed zombie null spawn locations + added warning if it happens

--- Assets/Scripts/Zombies/ZombieWave.cs
+++ Assets/Scripts/Zombies/ZombieWave.cs
@@ -13,28 +13,35 @@ public class ZombieWave : MonoBehaviour {
 		for( int i=0; i < spawnLocations.Count; i++ )
 		{
 			locationObject = spawnLocations[i];
-			Vector3 relativePos = new Vector3( 0 , 0 , 1f );
-			Vector3 exactPos = locationObject.transform.TransformPoint(relativePos);
-
-			ZombieSpawnData zsd = locationObject.GetComponent<ZombieSpawnData>();
-			switch (zsd.spawnType)
+			if( locationObject != null )
 			{
-			case ZombieSpawnType.BurrowUp:
-				Gizmos.color = Color.blue;
-				break;
-			case ZombieSpawnType.StandUpFromBack:
-				Gizmos.color = Color.yellow;
-				break;
-			case ZombieSpawnType.Walk:
-				Gizmos.color = Color.green;
-				break;
-			case ZombieSpawnType.Crawl:
-				Gizmos.color = Color.red;
-				break;
+				Vector3 relativePos = new Vector3( 0 , 0 , 1f );
+				Vector3 exactPos = locationObject.transform.TransformPoint(relativePos);
+	
+				ZombieSpawnData zsd = locationObject.GetComponent<ZombieSpawnData>();
+				switch (zsd.spawnType)
+				{
+				case ZombieSpawnType.BurrowUp:
+					Gizmos.color = Color.blue;
+					break;
+				case ZombieSpawnType.StandUpFromBack:
+					Gizmos.color = Color.yellow;
+					break;
+				case ZombieSpawnType.Walk:
+					Gizmos.color = Color.green;
+					break;
+				case ZombieSpawnType.Crawl:
+					Gizmos.color = Color.red;
+					break;
+				}
+				Gizmos.DrawSphere( locationObject.transform.position, 0.06f);
+	
+				Gizmos.DrawLine ( locationObject.transform.position, exactPos );
 			}
-			Gizmos.DrawSphere( locationObject.transform.position, 0.06f);
-
-			Gizmos.DrawLine ( locationObject.transform.position, exactPos );
+			else
+			{
+				Debug.LogWarning("ZombieWave-one of the spawn locations used by " + this.transform.parent.name + " is null." );
+			}
 		}
 	}
 }
