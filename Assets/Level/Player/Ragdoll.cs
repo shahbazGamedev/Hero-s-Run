77c3f482c2042d8844c280fe0b59e52121a86636
korbain <regis.g@videotron.ca>
2017-01-19 20:37:37 +0000

    Deleted duplicate blob shadows created rag doll script

---
+++ Assets/Level/Player/Ragdoll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class Ragdoll : MonoBehaviour {
+  
+Component[] bones;
+Animator anim;
+
+	void Start ()
+	{
+		bones = gameObject.GetComponentsInChildren<Rigidbody> (); 
+		anim = GetComponent<Animator> ();
+	}
+
+	public void controllRagdoll ( bool enableRagdoll ) 
+	{
+		Debug.Log("controllRagdoll-number of bones: " + bones.Length );
+		foreach (Rigidbody ragdoll in bones)
+		{
+			ragdoll.isKinematic = enableRagdoll;
+
+		}
+	
+		anim.enabled = enableRagdoll;
+	}
+		
+}
