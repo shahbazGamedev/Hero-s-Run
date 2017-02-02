7aaa4c3d9354b5355e20332b427f507f7cfc0e6d
korbain <regis.g@videotron.ca>
2017-02-02 15:13:50 +0000

    Hero selection quick and dirty left and right arrows

--- Assets/Scripts/HeroCarousel.cs
+++ Assets/Scripts/HeroCarousel.cs
@@ -1,12 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroCarousel : MonoBehaviour {
 
+	ScrollRect scrollRect;
+	int currentIndex = 0;
+	const float STEP_VALUE = 1f/3f;
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		
+		scrollRect = GetComponent<ScrollRect>();
+	}
+
+	public void previousItem()
+	{
+		currentIndex--;
+		if( currentIndex < 0 ) currentIndex = 3;
+		scrollRect.horizontalNormalizedPosition = currentIndex * STEP_VALUE;
+	}
+
+	public void nextItem()
+	{
+		currentIndex++;
+		if( currentIndex > 3 )
+		{
+			currentIndex = 0;
+		}
+		scrollRect.horizontalNormalizedPosition = currentIndex * STEP_VALUE;
+	}
+
+	public void normalizedPosition ( Vector2 value )
+	{
+		//print("normalizedPosition " + value.x );
 	}
 	
 	// Update is called once per frame
