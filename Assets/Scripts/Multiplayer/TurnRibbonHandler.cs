7af702397461e7a4e7dbb5906206c3de3676a4df
korbain <regis.g@videotron.ca>
2017-02-27 22:50:52 +0000

    first pass of card handling in turn ribbon - has bugs

--- Assets/Scripts/Multiplayer/TurnRibbonHandler.cs
+++ Assets/Scripts/Multiplayer/TurnRibbonHandler.cs
@@ -14,14 +14,17 @@ public class TurnRibbonHandler : MonoBehaviour {
 	[Header("Next Card")]
 	[SerializeField] Image nextCardImage;
 	[SerializeField] Text nextCardText;
+	CardManager.CardData nextCard;
 	[Header("Mana Bar")]
 	[SerializeField] ManaBar manaBar;
+
+	const int NUMBER_CARDS_IN_BATTLE_DECK = 8;
 
 	int [] cardIndexArray = new int[]{0,1,2,3,4,5,6,7};
 	List<int> cardIndexList = new List<int>(8);
 	List<CardManager.CardData> turnRibbonList = new List<CardManager.CardData>();
 	List<Button> turnRibbonButtonList = new List<Button>();
-	CardManager.CardData nextCard;
+	Queue<CardManager.CardData> cardQueue = new Queue<CardManager.CardData>();
 
 	// Use this for initialization
 	void Start ()
@@ -37,6 +40,15 @@ public class TurnRibbonHandler : MonoBehaviour {
 		}
 		//Set next card with a unique, random card
 		setNextCard( battleDeckList[getUniqueRandom()] );
+
+		//We are testing with 3 and with 4 cards in the turn ribbon
+		//So we are either queuing 4 cards: 8 - 3 - 1 or 3 cards: 8 - 4 - 1
+		int cardsToQueue = NUMBER_CARDS_IN_BATTLE_DECK - CardManager.Instance.cardsInTurnRibbon - 1;
+		for( int i = 0; i < cardsToQueue; i++ )
+		{
+			addCardToQueue(battleDeckList[getUniqueRandom()]);
+		}
+
 	}
 
 	void addCardToTurnRibbon( PlayerDeck.PlayerCardData card )
@@ -45,8 +57,8 @@ public class TurnRibbonHandler : MonoBehaviour {
 		go.transform.SetParent(cardPanel,false);
 		Button cardButton = go.GetComponent<Button>();
 		turnRibbonButtonList.Add(cardButton);
-		cardButton.onClick.RemoveListener(() => OnClickPlayerIcon(card));
-		cardButton.onClick.AddListener(() => OnClickPlayerIcon(card));
+		cardButton.onClick.RemoveListener(() => OnClickCard(card));
+		cardButton.onClick.AddListener(() => OnClickCard(card));
 		Image cardImage = go.GetComponent<Image>();
 		CardManager.CardData cardData = CardManager.Instance.getCardByName( card.name );
 		cardImage.sprite = cardData.icon;
@@ -62,6 +74,12 @@ public class TurnRibbonHandler : MonoBehaviour {
 		this.nextCard = cardData;
 	}
 
+	void addCardToQueue( PlayerDeck.PlayerCardData cardToQueue )
+	{
+		CardManager.CardData cardData = CardManager.Instance.getCardByName( cardToQueue.name );
+		cardQueue.Enqueue( cardData );
+	}
+
 	void Update()
 	{
 		//If we don't have enough mana to play a card, make it non-interactable
@@ -71,24 +89,38 @@ public class TurnRibbonHandler : MonoBehaviour {
 		}
 	}
 
-	public void OnClickPlayerIcon( PlayerDeck.PlayerCardData card )
+	public void OnClickCard( PlayerDeck.PlayerCardData card )
 	{
-		print("OnClickPlayerIcon");
-		RectTransform onSelectRectTransform = onSelectButton.GetComponent<RectTransform>();
+		/*RectTransform onSelectRectTransform = onSelectButton.GetComponent<RectTransform>();
 		onSelectRectTransform.localScale = Vector3.one;
-
 		//Position on select game object on top of the selected entry
 		onSelectButton.transform.SetParent( card.rectTransform, false );
 		onSelectButton.SetActive( true );
-
-		//Copy the icon and name
+		scaleUp();*/
+		//Copy the icon
 		CardManager.CardData cardData = CardManager.Instance.getCardByName( card.name );
-		onSelectPlayerIcon.sprite = cardData.icon;
-
-		scaleUp();
+		//onSelectPlayerIcon.sprite = cardData.icon;
 
 		manaBar.deductMana( cardData.manaCost );
 
+		//Play the card effect
+		playCardEffect( cardData.name );
+		//Replace the card played by the card held in Next
+		int indexOfCardPlayed = turnRibbonList.FindIndex( element => element == cardData );
+		Button buttonOfCardPlayed = turnRibbonButtonList[indexOfCardPlayed];
+		buttonOfCardPlayed.GetComponent<Image>().sprite = nextCard.icon;
+		//Dequeue the oldest card and place it in Next
+		CardManager.CardData oldestCardInQueue = cardQueue.Dequeue();
+		nextCard = oldestCardInQueue;
+		nextCardImage.sprite = oldestCardInQueue.icon;
+		//Enqueue the card that was just played
+		cardQueue.Enqueue( cardData );
+	}
+
+	void playCardEffect( string cardName )
+	{
+		//To be implemented
+		Debug.Log("TurnRibbonHandler-playCardEffect: playing effect for card: " + cardName );
 	}
 
 	void scaleUp()
