13171227f29d78cea5b5077c52441e97ca9eb21d
korbain <regis.g@videotron.ca>
2017-03-25 14:13:10 +0000

    Jump pad - unfinished color vex changer

--- Assets/Scripts/Level/JumpPad.cs
+++ Assets/Scripts/Level/JumpPad.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour {
 
+	[SerializeField] GameObject Teleport_VFX1;
+	[SerializeField] GameObject Teleport_VFX2;
+	Color originalColor;
+
+	void Awake()
+	{
+		originalColor = Teleport_VFX1.GetComponent<MeshRenderer>().material.GetColor("_TintColor");
+	}
+
+	void changeColor( Color newColor )
+	{
+		Teleport_VFX1.GetComponent<MeshRenderer>().material.SetColor("_TintColor", newColor);
+		Teleport_VFX2.GetComponent<MeshRenderer>().material.SetColor("_TintColor", newColor);
+	}
+
+	void resetColor()
+	{
+		Teleport_VFX1.GetComponent<MeshRenderer>().material.SetColor("_TintColor", originalColor);
+		Teleport_VFX2.GetComponent<MeshRenderer>().material.SetColor("_TintColor", originalColor);
+	}
+
 	void OnTriggerEnter(Collider other)
 	{
 		if( other.gameObject.CompareTag("Player")  )
