10f484436c2095e543e8d1463bfeec7047285362
korbain <regis.g@videotron.ca>
2017-05-06 18:06:06 +0100

    First pass on store

---
+++ Assets/Scripts/Store/StoreEntryForCoins.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoreEntryForCoins : MonoBehaviour {
+
+	[Header("Store Entry For Coins")]
+	[SerializeField]  Text title;
+	[SerializeField]  string titleID;
+	[Tooltip("For example, the number of coins you get with your purchase.")]
+	[SerializeField]  Text quantityText;
+	[SerializeField]  int quantity;
+	[SerializeField] Button buyWithGemsButton;
+	[SerializeField] Text buyWithGemsButtonLabel;
+	[SerializeField] int priceInGems;
+
+	// Use this for initialization
+	void Start ()
+	{
+		title.text = LocalizationManager.Instance.getText(titleID);
+		buyWithGemsButtonLabel.text = priceInGems.ToString("N0");
+		quantityText.text = quantity.ToString("N0");
+		if( priceInGems <= GameManager.Instance.playerInventory.getGemBalance() )
+		{
+			//We can afford it.
+			buyWithGemsButtonLabel.color = Color.white;
+		}
+		else
+		{
+			//We can't afford it.
+			buyWithGemsButtonLabel.color = Color.red;
+		}
+	}
+
+	public void buy()
+	{
+		Debug.Log("Buy " + quantity + " coins.");
+		UISoundManager.uiSoundManager.playButtonClick();
+		if( priceInGems <= GameManager.Instance.playerInventory.getGemBalance() )
+		{
+			//We can afford it.
+			GameManager.Instance.playerInventory.deductGems( priceInGems );
+			GameManager.Instance.playerInventory.addCoins( quantity );
+		}
+		else
+		{
+			//We can't afford it. Offer to go to shop.
+		}
+	}
+}
---
+++ Assets/Scripts/Store/StoreEntryForGems.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class StoreEntryForGems : MonoBehaviour {
+
+	[Header("Store Entry For Gems")]
+	[SerializeField]  Text title;
+	[SerializeField]  string titleID;
+	[Tooltip("For example, the number of gems you get with your purchase. Gems are bought with real money.")]
+	[SerializeField]  Text quantityText;
+	[SerializeField]  int quantity;
+	[SerializeField]  Button buyGemsButton;	
+	[SerializeField]  Text buyGemsButtonLabel;
+	[Tooltip("Gems price will ultimately come from the app store. For now, we use dummy values.")]
+	[SerializeField]  float price;
+
+	string currencySymbol = "$";
+
+	// Use this for initialization
+	void Awake ()
+	{
+		title.text = LocalizationManager.Instance.getText(titleID);
+		buyGemsButtonLabel.text = currencySymbol + price.ToString();
+		quantityText.text = quantity.ToString("N0");
+	}
+
+	public void buy()
+	{
+		Debug.Log("Buy " + quantity + " gems.");
+		UISoundManager.uiSoundManager.playButtonClick();
+		GameManager.Instance.playerInventory.addGems( quantity );
+	}
+}
