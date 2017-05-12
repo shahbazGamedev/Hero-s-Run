ed4b8938cabaa9c60a83bb48cbf93431893dc9dd
korbain <regis.g@videotron.ca>
2017-05-12 17:00:34 +0100

    Added fx to trip mine

--- Assets/Scripts/Multiplayer/Photon/Cards/Card Fx Prefabs/Resources/TripMine.cs
+++ Assets/Scripts/Multiplayer/Photon/Cards/Card Fx Prefabs/Resources/TripMine.cs
@@ -4,14 +4,7 @@ using System.Collections;
 
 public class TripMine : CardSpawnedObject {
 	
-	void OnTriggerEnter(Collider other)
-	{
-		if( other.gameObject.CompareTag("Player") && other.gameObject.name != casterName )
-		{
-			destroyAllTargetsWithinBlastRadius( 10f, true );
-			GameObject.Destroy( gameObject );
-		}
-	}
+	[SerializeField] ParticleSystem explosionEffect;
 
 	void OnPhotonInstantiate( PhotonMessageInfo info )
 	{
@@ -26,6 +19,23 @@ public class TripMine : CardSpawnedObject {
 
 		//Position the trip mine flush with the ground and try to center it in the middle of the road if possible.
 		positionSpawnedObject( 0 );
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if( other.gameObject.CompareTag("Player") && other.gameObject.name != casterName )
+		{
+			destroyAllTargetsWithinBlastRadius( 10f, true );
+			explode();
+			GameObject.Destroy( gameObject );
+		}
+	}
+
+	void explode()
+	{
+		ParticleSystem effect = GameObject.Instantiate( explosionEffect, transform.position, transform.rotation );
+		effect.GetComponent<AudioSource>().Play ();
+		effect.Play();
 	}
 
 }
