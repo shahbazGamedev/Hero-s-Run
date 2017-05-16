7a6c2f64088e1b835e1fa83e4a3aa75ba1452159
korbain <regis.g@videotron.ca>
2017-05-16 19:50:35 +0100

    Circuit Selection - deleted online players count
    since no longer relevant

--- Assets/Scripts/Multiplayer/CarouselEntry.cs
+++ Assets/Scripts/Multiplayer/CarouselEntry.cs
@@ -11,9 +11,6 @@ public class CarouselEntry : MonoBehaviour {
 	public Text circuitName;
 	public Image circuitImage;
 
-	[Header("Online Players")]
-	[SerializeField] Text numberOnlinePlayers;
-
 	[SerializeField] Text raceButtonText;
 
 	bool levelLoading = false;
@@ -31,28 +28,6 @@ public class CarouselEntry : MonoBehaviour {
 
 		//Common to all carousel entries
 		raceButtonText.text = LocalizationManager.Instance.getText( "MENU_CONFIRM" );
-	}
-
-	void Start ()
-	{
-		InvokeRepeating("getNumberOfOnlinePlayers", 0f, 5f );
-	}
-	
-	void getNumberOfOnlinePlayers()
-	{
-		//The player needs to be connected to the Internet, connected to Photon and not be in offline mode.
-		//Note: if PhotonNetwork.offlineMode is true, PhotonNetwork.connected will also be true, hence the extra test.
-		if( Application.internetReachability != NetworkReachability.NotReachable && PhotonNetwork.connected && !PhotonNetwork.offlineMode )
-		{
-			//The count of players currently using this application (available on MasterServer in 5sec intervals).
-			//This is the total for ALL tracks.
-			numberOnlinePlayers.text = PhotonNetwork.countOfPlayers.ToString();
-
-		}
-		else
-		{
-			numberOnlinePlayers.text = LocalizationManager.Instance.getText( "CIRCUIT_NOT_AVAILABLE" );
-		}
 	}
 
 	public void OnClickShowMatchmaking()
