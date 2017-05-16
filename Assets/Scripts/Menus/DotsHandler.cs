98c865f617966ce9de9914fd5df9455afc7d2883
korbain <regis.g@videotron.ca>
2017-05-16 11:56:06 +0100

    DotsHandler can handle an arbitrary number of panels

--- Assets/Scripts/Menus/DotsHandler.cs
+++ Assets/Scripts/Menus/DotsHandler.cs
@@ -7,70 +7,36 @@ using System;
 public class DotsHandler : MonoBehaviour {
 
 	[SerializeField] List<Image> dotsList = new List<Image>();
-	public int activePanel; //0 means left most, 1 means center or rightmost, 2 means rightmost
+	[SerializeField] bool travelsLeft = true;
+	public int activePanel;
 
 	void Start()
-	{
-		if( dotsList.Count == 2 )
+	{ 
+		if( travelsLeft )
+		{
+			activePanel = dotsList.Count - 1;
+		}
+		else
 		{
 			activePanel = 0;
-		}
-		else if( dotsList.Count == 3 )
-		{
-			activePanel = 2;
 		}
 	}
 
 	public void OnValueChanged( Vector2 value )
 	{
 		float xPosition = (float) Math.Round( value.x, 1 );
-		//print("value.x " + xPosition );
-
-		for( int i = 0; i < dotsList.Count; i ++ )
+		float increment = 1f/(dotsList.Count-1);
+		for( float i = 0; i <= 1f; i = i + increment )
 		{
-			if( dotsList.Count == 2 )
+			int index = (int) (i * (dotsList.Count-1));
+			if( i == xPosition )
 			{
-				//leftmost entry
-				if( xPosition == 0 ) 					
-				{
-					dotsList[0].color = Color.white;
-					activePanel = 0;
-				}
-				//rightmost entry
-				else if( xPosition == 1f ) 			
-				{
-					dotsList[1].color = Color.white;
-					activePanel = 1;
-				}
-				else
-				{
-					dotsList[i].color = Color.gray;
-				}
+				dotsList[index].color = Color.white;
+				activePanel = index;
 			}
-			else if( dotsList.Count == 3 )
+			else			
 			{
-				//leftmost entry
-				if( xPosition == 0 )
-				{
-					dotsList[0].color = Color.white;
-					activePanel = 0;
-				}
-				//middle entry
-				else if( xPosition == 0.5f )
-				{
-					dotsList[1].color = Color.white;
-					activePanel = 1;
-				}
-				//rightmost entry
-				else if( xPosition == 1f )
-				{
-					dotsList[2].color = Color.white;
-					activePanel = 2;
-				}
-				else
-				{
-					dotsList[i].color = Color.gray;
-				}
+				dotsList[index].color = Color.gray;
 			}
 		}
 	}
