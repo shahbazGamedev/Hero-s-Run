0cf1e29b1df998580c4862df3c68be979cf261eb
korbain <regis.g@videotron.ca>
2017-05-23 15:11:44 +0100

    Game tips are now localized

--- Assets/Scripts/Multiplayer/TipHandler.cs
+++ Assets/Scripts/Multiplayer/TipHandler.cs
@@ -1,43 +1,29 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class TipHandler : MonoBehaviour {
 
 	[Header("Game Tips")]
 	[SerializeField] TextMeshProUGUI tipDescription;
-	List<string> tipList = new List<string>();
+	
+	[Tooltip("The number of tips contained in GameText. The text ID should be in the form TIP_<number>. The first tip is TIP_1.")]
+	[SerializeField] int numberOfTips;
 
 	void Start ()
 	{
-		populateTipList();
 		displayRandomTip ();
 	}
 
-	void populateTipList()
-	{
-		string tip = "You can plough through an ice wall with raging Bull.";
-		tipList.Add(tip);
-		tip = "A player hit with Statis will lose his Sentry bot if he has one.";
-		tipList.Add(tip);
-		tip = "You can't play cards while ziplining.";
-		tipList.Add(tip);
-		tip = "You can invite a friend to race if you know their user name.";
-		tipList.Add(tip);
-	} 
-
+	/// <summary>
+	/// Displays a localized random tip.
+	/// The GameText should contain the tips.
+	/// The text ID should be in the form TIP_<number>.
+	/// The first tip is TIP_1.
+	/// </summary>
 	void displayRandomTip ()
 	{
-		if( tipList.Count > 0 )
-		{
-			tipDescription.text = tipList[ Random.Range(0, tipList.Count) ];
-		}
-		else
-		{
-			Debug.LogError("TipHandler-the tip list is empty.");
-			tipDescription.text = string.Empty;
-		}
+		int randomNumber = Random.Range(1, numberOfTips);
+		tipDescription.text = LocalizationManager.Instance.getText( "TIP_" + randomNumber.ToString() );
 	}
 }
