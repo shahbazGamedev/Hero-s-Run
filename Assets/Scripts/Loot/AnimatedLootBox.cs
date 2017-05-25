21c68f88f2e9b7357715a768eb2a1c3eecf68667
korbain <regis.g@videotron.ca>
2017-05-25 19:36:02 +0100

    New RadialTimerButton for loot box

---
+++ Assets/Scripts/Loot/AnimatedLootBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AnimatedLootBox : MonoBehaviour {
+
+	[SerializeField] GameObject lid;
+	bool isOpen = false;
+
+	public void Open ()
+	{
+		if( isOpen ) return;
+		isOpen = true;
+		LeanTween.rotateX( lid, -60f, 0.4f ).setEaseOutCubic();
+		GetComponent<AudioSource>().Play();
+	}
+	
+}
---
+++ Assets/Scripts/Menus/RadialTimerButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RadialTimerButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+
+	[SerializeField] Image radialImageMask;
+	[SerializeField] float duration = 2f;
+
+	void Awake()
+	{
+		//The button should not be interactable.
+		//If it were interactable, you could get an OnClick event if the player released the button even if the timer had not completed.
+		GetComponent<Button>().interactable = false;
+	}
+
+	public void OnPointerDown(PointerEventData eventData )
+	{
+		StartCoroutine( animate() );
+	}
+
+	IEnumerator animate()
+	{
+		float startTime = Time.time;
+		float elapsedTime = 0;
+
+		while ( elapsedTime <= duration )
+		{
+			elapsedTime = Time.time - startTime;
+
+			radialImageMask.fillAmount =  Mathf.Lerp( 0, 1f, elapsedTime/duration );
+			yield return new WaitForEndOfFrame();  
+	    }
+		radialImageMask.fillAmount =  0;
+		CallOnClick();
+	}
+
+	public void OnPointerUp(PointerEventData eventData )
+	{
+		StopAllCoroutines();
+		radialImageMask.fillAmount =  0;
+	}
+
+	void CallOnClick()
+	{
+		GetComponent<Button>().interactable = true;
+		PointerEventData pointer = new PointerEventData(EventSystem.current);
+		ExecuteEvents.Execute( gameObject, pointer, ExecuteEvents.pointerClickHandler);
+		GetComponent<Button>().interactable = false;
+	}
+
+}
