555c996276edae92fbb3310038cfd51ad51d80dd
korbain <regis.g@videotron.ca>
2017-05-27 08:05:23 +0100

    Quick and dirty display of loot received

--- Assets/Scripts/Loot/LootBoxMenu.cs
+++ Assets/Scripts/Loot/LootBoxMenu.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LootBoxMenu : MonoBehaviour {
+
+	[SerializeField] GameObject lootPanel;
+	[SerializeField] Image lootSprite;
+	[SerializeField] TextMeshProUGUI lootContentText;
+	[SerializeField] Sprite coinSprite;
+	[SerializeField] Sprite gemSprite;
 
 	// Use this for initialization
 	void Start ()
@@ -23,10 +31,10 @@ public class LootBoxMenu : MonoBehaviour {
 	public void LootBoxGrantedEvent( LootBox lootBox )
 	{
 		lootBox.print();
-		giveLootBoxContent( lootBox );
+		StartCoroutine( giveLootBoxContent( lootBox ) );
 	}
 
-	void giveLootBoxContent( LootBox lootBox )
+	IEnumerator giveLootBoxContent( LootBox lootBox )
 	{
 		List<LootBox.Loot> lootList = lootBox.getLootList();
 		for( int i = 0; i < lootList.Count; i++ )
@@ -35,28 +43,42 @@ public class LootBoxMenu : MonoBehaviour {
 			{
 				case LootType.COINS:
 					GameManager.Instance.playerInventory.addCoins( lootList[i].quantity );
+					displayLootReceived( "+" + lootList[i].quantity.ToString(), coinSprite );
 				break;
 
 				case LootType.GEMS:
 					GameManager.Instance.playerInventory.addGems( lootList[i].quantity );
+					displayLootReceived( "+" + lootList[i].quantity.ToString(), gemSprite );
 				break;
 
 				case LootType.CARDS:
 					GameManager.Instance.playerDeck.addCardFromLootBox( lootList[i].cardName, lootList[i].quantity );
+					CardManager.CardData cd = CardManager.Instance.getCardByName( lootList[i].cardName );
+					displayLootReceived( "+" + lootList[i].quantity.ToString(), cd.icon );
 				break;
 
 				case LootType.PLAYER_ICON:
 					GameManager.Instance.playerIcons.unlockPlayerIcon( lootList[i].uniqueItemID );
+					string iconName = LocalizationManager.Instance.getText( "PLAYER_ICON_" + lootList[i].uniqueItemID.ToString() );
+					displayLootReceived( iconName, ProgressionManager.Instance.getPlayerIconSpriteByUniqueId( lootList[i].uniqueItemID ).icon );
 				break;
 				
 				default:
 					Debug.LogError("Give loot content encountered an unknown loot type: " + lootList[i].type );
 				break;
 			}
+			yield return new WaitForSecondsRealtime( 3f );
 		}
 		//Save
 		GameManager.Instance.playerInventory.serializePlayerInventory();
 		GameManager.Instance.playerDeck.serializePlayerDeck(true);
+		lootPanel.SetActive (false );
 	}
 
+	void displayLootReceived( string text, Sprite sprite )
+	{
+		lootSprite.sprite = sprite;
+		lootContentText.text = text;
+		lootPanel.SetActive (true );
+	}
 }
