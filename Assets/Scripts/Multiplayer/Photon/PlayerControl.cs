e160bbc23b5be223d6a86ef5a890a1b361badb56
korbain <regis.g@videotron.ca>
2017-07-02 20:39:30 -0400

    Changed Double Jump Speed to 18

--- Assets/Scripts/Multiplayer/Photon/PlayerControl.cs
+++ Assets/Scripts/Multiplayer/Photon/PlayerControl.cs
@@ -118,7 +118,7 @@ public class PlayerControl : Photon.PunBehaviour {
 	bool jumping = false;
 	bool doingDoubleJump = false;
 	float jumpSpeed = 12f;
-	float doubleJumpSpeed = 20f;
+	float doubleJumpSpeed = 18f;
 	float DOUBLE_JUMP_RUN_SPEED = 1f; //We want the player to leap forward during a double jump
 	float MAX_RUN_SPEED_FOR_DOUBLE_JUMP = 20f; //We want to cap the maximum run speed during a double jump. If the player is sprinting for example, we don't want him to leap into a wall.
 	public float distanceToGround = 0;
