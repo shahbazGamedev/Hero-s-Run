7e83f4de68625837b52ffcd50f16dea8b7a41981
korbain <regis.g@videotron.ca>
2017-07-07 20:05:15 -0400

    Finished flamethrower obstacle

---
+++ Assets/Scripts/Level/Flamethrower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//The collider should have the "Obstacle_M" tag.
+public class Flamethrower : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if( other.CompareTag("Player") )
+		{
+			other.GetComponent<PlayerControl>().killPlayer ( DeathType.Flame );
+		}
+	}
+
+}
