c2ff1d3bf3505a856928848475c30836d5f6c11e
korbain <regis.g@videotron.ca>
2017-07-18 08:07:00 -0400

    Sci-Fi doors, changed end tile to use doors

---
+++ Assets/Scripts/Level/DayTimeActivator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayTimeActivator : MonoBehaviour {
+
+	[SerializeField] SunType activateForThisSunType = SunType.Sky_city_night;
+
+	void Start ()
+	{
+		SunType sunType = LevelManager.Instance.getSelectedCircuit().sunType;
+		if( sunType == activateForThisSunType )
+		{
+			gameObject.SetActive( true );
+			print("DayTimeActivator " + sunType + " " + name );
+		}
+		else
+		{
+			gameObject.SetActive( false );
+		}
+	}
+	
+}
