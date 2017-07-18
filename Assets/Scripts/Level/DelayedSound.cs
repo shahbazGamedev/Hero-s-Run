d848cd2de4755394f00d8840ac43a17749514076
korbain <regis.g@videotron.ca>
2017-07-18 09:14:05 -0400

    Added sound for spacecrafts

---
+++ Assets/Scripts/Level/DelayedSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DelayedSound : MonoBehaviour {
+
+	[SerializeField] float delayBeforeSoundPlays = 3f;
+
+	void Start ()
+	{
+		GetComponent<AudioSource>().PlayDelayed( delayBeforeSoundPlays );
+	}
+	
+}
