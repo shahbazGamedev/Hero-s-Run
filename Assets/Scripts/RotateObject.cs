a595f8157de7379a343b8f7a09b05cf238f8b57c
korbain <regis.g@videotron.ca>
2017-09-01 17:09:43 -0400

    New Matchmaking and small update in deck

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+[SerializeField] float rotationSpeed = 1.5f;  //This will determine max rotation speed, you can adjust in the inspector
+
+	void Update()
+	{
+		//If you want to prevent rotation, just don't call this method
+		rotate();
+	}
+	
+	void rotate()
+	{
+		transform.Rotate(-Camera.main.transform.up, Input.GetAxis("Mouse X") * rotationSpeed, Space.World);
+	}
+}
