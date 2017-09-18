8bab61aece1e91a661b81ed9b5fb1c3112972e65
korbain <regis.g@videotron.ca>
2017-09-18 11:03:04 -0400

    Big refactor of player IK

--- Assets/Scripts/Multiplayer/Photon/PlayerIK.cs
+++ Assets/Scripts/Multiplayer/Photon/PlayerIK.cs
@@ -11,8 +11,6 @@ using UnityEngine;
 public class PlayerIK : MonoBehaviour {
 
 	[Header("Look At IK")]
-	public Transform lookAtTarget;
-	Animator anim;
 	[SerializeField] float lookAtWeight = 0.8f;
 	[SerializeField] float bodyWeight = 0.7f;
 	[SerializeField] float headWeight = 0.9f;
@@ -20,13 +18,30 @@ public class PlayerIK : MonoBehaviour {
 	[SerializeField] float clampWeight = 1f;
 	[SerializeField] float activeDistanceSquared = 50f * 50f;
 	[SerializeField] float dotProductIK = 0.58f;
-	public bool lookAtActive = false;
-	public bool enableIK = true; //this could be used to disable IK in case the rig does not support it or the performance on the device suffers
+
+	Transform lookAtTarget;
+	Animator anim;
 	Vector3 playerOffset = new Vector3( 0, 1.4f, 0 ); //Look at the player's eyes, not his feet.
+	//NOTE: Your reference to a coroutine will NOT become null once the coroutine has finished running or even if you called StopCoroutine on it.
+	Coroutine fadeInLookAtCoroutine;
+	Coroutine fadeOutLookAtCoroutine;
+	//We want the IK to be disabled at the beginning of the race. It looks weird when the opponents stare at the player right at the get go.
+	bool isIKEnabled = false;
+	const float DELAY_BEFORE_ACTIVATING_IK = 10f;
 
 	void Awake ()
 	{
 		anim = GetComponent<Animator>();
+	}
+
+	void Start ()
+	{
+		Invoke( "activateIKAfterDelay", DELAY_BEFORE_ACTIVATING_IK );
+	}
+
+	void activateIKAfterDelay()
+	{
+		isIKEnabled = true;
 	}
 
 	/*
@@ -44,96 +59,124 @@ public class PlayerIK : MonoBehaviour {
 
 	void OnAnimatorIK()
 	{
-		if( lookAtTarget != null && enableIK )
+		//Is there an active look-at fade in or fade out?
+		if( fadeInLookAtCoroutine != null || fadeOutLookAtCoroutine != null )
 		{
-			float distance = Vector3.SqrMagnitude(lookAtTarget.position - transform.position);
-			if( distance < activeDistanceSquared && getDotProduct() > dotProductIK )			
+			//If we have a target, check that it is still within range.
+			if( lookAtTarget != null )
 			{
-				anim.SetLookAtPosition( lookAtTarget.TransformPoint( playerOffset ) );
-				anim.SetLookAtWeight( lookAtWeight, bodyWeight, headWeight, eyesWeight, clampWeight );
+				float distance = Vector3.SqrMagnitude(lookAtTarget.position - transform.position);
+				if( distance < activeDistanceSquared && getDotProduct() > dotProductIK )			
+				{
+					//The target is in range.
+					anim.SetLookAtPosition( lookAtTarget.TransformPoint( playerOffset ) );
+				}
+				else
+				{
+					//The target is no longer in range.
+					//Stop the fade in look-at.
+					if( fadeInLookAtCoroutine != null ) StopCoroutine( fadeInLookAtCoroutine );
+					lookAtTarget = null;
+					//Start the fade out look-at
+					if( fadeOutLookAtCoroutine == null )
+					{
+						fadeOutLookAtCoroutine = StartCoroutine( fadeOutLookAt( 0, 0.9f ) );
+					}								
+				}
 			}
-			else
-			{
-				//Look-at target is no longer in range
-				StartCoroutine( fadeOutLookAtPosition( 0.2f, 0.9f ) );									
-			}
+			//In all cases, update the look-at weight
+			anim.SetLookAtWeight( lookAtWeight, bodyWeight, headWeight, eyesWeight, clampWeight );
 		}
 	}
 
-	IEnumerator setLookAtTarget( float activationDelay, Transform lookAtTarget )
+	void setLookAtTarget( float activationDelay, Transform lookAtTarget )
 	{
-		//Ignore null targets
-		if( lookAtTarget == null  ) yield break;
+		//Ignore if IK is not enabled.
+		if( !isIKEnabled ) return;
 
-		//print( name + " has a look-at target of " + lookAtTarget.name );
+		//Ignore if the target is null.
+		if( lookAtTarget == null  ) return;
+
+		//Ignore yourself.
 		if( transform == lookAtTarget )
 		{
  			Debug.LogError("PlayerIK error: you should not set the look-at target to be yourself: " + name );
-			yield break;
+			return;
 		}
 
+		//Ignore if you are in IDLE.
+		if( GetComponent<PlayerControl>().getCharacterState() == PlayerCharacterState.Idle ) return;
+
 		//Don't interrupt the current look-at if one is active
-		if( lookAtActive ) yield break;
+		if( fadeInLookAtCoroutine != null ) return;
 
 		//Don't do anything if the proposed target is too far
 		float distance = Vector3.SqrMagnitude(lookAtTarget.position - transform.position);
-		if( distance > activeDistanceSquared ) yield break;		
+		if( distance > activeDistanceSquared ) return;		
+
+		this.lookAtTarget = lookAtTarget;
+
+		fadeInLookAtCoroutine = StartCoroutine( fadeInLookAt( activationDelay, 0.8f, 0.6f, 1.2f, 0, 0.8f ) );
+	}
+
+	IEnumerator fadeInLookAt( float activationDelay, float fadeInFinalWeight, float fadeInDuration, float stayDuration, float fadeOutFinalWeight, float fadeOutDuration )
+	{
+		Debug.Log( name + " has a look-at target of " + lookAtTarget.name );
 
 		//Wait before activating
 		yield return new WaitForSeconds(activationDelay);
 
-		this.lookAtTarget = lookAtTarget;
-		StartCoroutine( fadeInLookAtPosition( 0.8f, 0.6f, 1.2f ) );
-	}
-
-	IEnumerator fadeInLookAtPosition( float finalWeight, float fadeDuration, float stayDuration  )
-	{
-		lookAtActive = true;
-		float elapsedTime = 0;
-
 		//Fade in
-		elapsedTime = 0;
-		
+		float elapsedTime = 0;
 		float initialWeight = 0;
 		
 		do
 		{
 			elapsedTime = elapsedTime + Time.deltaTime;
-			lookAtWeight = Mathf.Lerp( initialWeight, finalWeight, elapsedTime/fadeDuration );
-			yield return new WaitForFixedUpdate();  
+			lookAtWeight = Mathf.Lerp( initialWeight, fadeInFinalWeight, elapsedTime/fadeInDuration );
+			yield return new WaitForEndOfFrame();  
 			
-		} while ( elapsedTime < fadeDuration );
+		} while ( elapsedTime < fadeInDuration );
 		
-		lookAtWeight = finalWeight;
+		lookAtWeight = fadeInFinalWeight;
 
 		//Stay
 		yield return new WaitForSeconds(stayDuration);
 
 		//Fade out
-		StartCoroutine( fadeOutLookAtPosition( 0.2f, 0.5f ) );
+		elapsedTime = 0;		
+		initialWeight = lookAtWeight;
+		
+		do
+		{
+			elapsedTime = elapsedTime + Time.deltaTime;
+			lookAtWeight = Mathf.Lerp( initialWeight, fadeOutFinalWeight, elapsedTime/fadeOutDuration );
+			yield return new WaitForFixedUpdate();  
+			
+		} while ( elapsedTime < fadeOutDuration );
+		
+		lookAtWeight = fadeOutFinalWeight;
+		lookAtTarget = null;
+		fadeInLookAtCoroutine = null;
 	
 	}
 
-	IEnumerator fadeOutLookAtPosition( float finalWeight, float fadeDuration )
-	{
-		float elapsedTime = 0;
-		
+	IEnumerator fadeOutLookAt( float fadeOutFinalWeight, float fadeOutDuration )
+	{ 
 		//Fade out
-		elapsedTime = 0;
-		
+		float elapsedTime = 0;
 		float initialWeight = lookAtWeight;
 		
 		do
 		{
 			elapsedTime = elapsedTime + Time.deltaTime;
-			lookAtWeight = Mathf.Lerp( initialWeight, finalWeight, elapsedTime/fadeDuration );
+			lookAtWeight = Mathf.Lerp( initialWeight, fadeOutFinalWeight, elapsedTime/fadeOutDuration );
 			yield return new WaitForFixedUpdate();  
 			
-		} while ( elapsedTime < fadeDuration );
+		} while ( elapsedTime < fadeOutDuration );
 		
-		lookAtWeight = finalWeight;
-		lookAtTarget = null;
-		lookAtActive = false;
+		lookAtWeight = fadeOutFinalWeight;
+		fadeOutLookAtCoroutine = null;
 	}
 
 	public void isOvertaking( int racePosition )
@@ -148,7 +191,7 @@ public class PlayerIK : MonoBehaviour {
 				//Now look-at the player after a short delay.
 				//We don't want the player to start looking at a right angle. We want the opponent to be
 				//about 0.05 * 20 (average run speed) = 1 meter in front before looking.
-				StartCoroutine( setLookAtTarget( 0.05f, playerOvertaking.transform ) );
+				setLookAtTarget( 0.05f, playerOvertaking.transform );
 			}
 			else
 			{
@@ -159,17 +202,14 @@ public class PlayerIK : MonoBehaviour {
 
 	public void playerDied()
 	{
-		StopCoroutine("setLookAtTarget");
+		if( fadeInLookAtCoroutine != null ) StopCoroutine(fadeInLookAtCoroutine);
+		lookAtTarget = null;
+		if( fadeOutLookAtCoroutine != null ) StopCoroutine(fadeOutLookAtCoroutine);
 
-		if( lookAtTarget != null && enableIK ) StartCoroutine( fadeOutLookAtPosition( 0.2f, 0.8f ) );
-
-		//Advise other players of death so that they can do a look-at
-		for( int i = 0; i < PlayerRace.players.Count; i ++ )
+		//If the look-at weight is not 0, we should return to 0.
+		if( lookAtWeight > 0 )
 		{
-			if( PlayerRace.players[i] != GetComponent<PlayerRace>() )
-			{
-				StartCoroutine( PlayerRace.players[i].GetComponent<PlayerIK>().setLookAtTarget( 0, transform ) );
-			}
+ 			fadeOutLookAtCoroutine = StartCoroutine( fadeOutLookAt( 0, 0.8f ) );
 		}
 	}
 
