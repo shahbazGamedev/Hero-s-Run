d6a1e022e723c216ef489624bacf992953732d4a
korbain <regis.g@videotron.ca>
2017-09-22 12:37:24 -0400

    Health bar marker is now animated

--- Assets/Scripts/Menus/HealthBarHandler.cs
+++ Assets/Scripts/Menus/HealthBarHandler.cs
@@ -9,9 +9,14 @@ public class HealthBarHandler : MonoBehaviour {
 	[SerializeField] GameObject healthBarHolder;	//Parent of the 3 radial images.
 	[SerializeField] Image lowHealthRadial; 		//The first 3 boxes (out of 10) are orange to indicate low health (30/100). The other 7 are gray.
 	[SerializeField] Image fullHealthRadial; 		//All 10 boxes are blue. Blue means healthy.
+	[SerializeField] RectTransform healthBarMarker;	//the vertical line. Anchor should be Left, Center.
 	[SerializeField] Image armorRadial;				//the armor indicator sits on top of the full health radial.
 	const float ANIMATION_DURATION_NORMAL = 0.8f;
 	const float ANIMATION_DURATION_FAST = 0.3f;
+
+	void Awake()
+	{
+	}
 
 	void OnEnable()
 	{
@@ -39,6 +44,7 @@ public class HealthBarHandler : MonoBehaviour {
 		{
 			//Player will die because his health is zero so animate super fast.
 			animationSpeed = ANIMATION_DURATION_FAST;
+			healthBarMarker.gameObject.SetActive( false );
 		}
 		else
 		{
@@ -46,20 +52,28 @@ public class HealthBarHandler : MonoBehaviour {
 		}
 		//Low and full radial images should always be in sync
 		float fillAmount = newHealth/(float)PlayerHealth.DEFAULT_HEALTH;
-		lowHealthRadial.GetComponent<UIAnimateRadialImage>().animateFillAmount( fillAmount, animationSpeed, onFinish );
-		fullHealthRadial.GetComponent<UIAnimateRadialImage>().animateFillAmount( fillAmount, animationSpeed, onFinish );
+		lowHealthRadial.GetComponent<UIAnimateRadialImage>().animateFillAmount( fillAmount, animationSpeed, healthBarMarker, onFinish );
+		fullHealthRadial.GetComponent<UIAnimateRadialImage>().animateFillAmount( fillAmount, animationSpeed, healthBarMarker, onFinish );
 	}
 
 	public void resetHealth ()
 	{
 		lowHealthRadial.fillAmount = 1f;			
-		fullHealthRadial.fillAmount = 1f;			
+		fullHealthRadial.fillAmount = 1f;
+		healthBarMarker.gameObject.SetActive( true );
+		positionHealthBarMarker( 1f );		
+	}
+
+	void positionHealthBarMarker( float fillAmount )
+	{
+		float healthBarLength = healthBarHolder.GetComponent<RectTransform>().sizeDelta.x;
+		healthBarMarker.anchoredPosition = new Vector2( fillAmount * healthBarLength, healthBarMarker.anchoredPosition.y );
 	}
 
 	public void changeArmor (int currentArmor, int newArmor, System.Action onFinish = null )
 	{
 		float fillAmount = newArmor/(float)PlayerHealth.MAXIMUM_ARMOR;
-		armorRadial.GetComponent<UIAnimateRadialImage>().animateFillAmount( newArmor, ANIMATION_DURATION_FAST, onFinish );
+		armorRadial.GetComponent<UIAnimateRadialImage>().animateFillAmount( newArmor, ANIMATION_DURATION_FAST, null, onFinish );
 	}
 
 	public void removeAllArmor ()
--- Assets/Scripts/Menus/UIAnimateRadialImage.cs
+++ Assets/Scripts/Menus/UIAnimateRadialImage.cs
@@ -5,21 +5,22 @@ using UnityEngine.UI;
 public class UIAnimateRadialImage : MonoBehaviour {
 
 	Image radialImage;
-	Coroutine fillAmountAnimateCoroutine;
+	Coroutine fillAmountAnimateCoroutine = null;
 
 	void Start ()
 	{
 		radialImage = GetComponent<Image>();
 	}
 	
-	public void animateFillAmount ( float toValue, float duration, System.Action onFinish = null )
+	public void animateFillAmount ( float toValue, float duration, RectTransform optionalMarker = null, System.Action onFinish = null )
 	{
 		if( fillAmountAnimateCoroutine != null ) StopCoroutine( fillAmountAnimateCoroutine );
-		fillAmountAnimateCoroutine = StartCoroutine( fillAmountAnimate( toValue, duration, onFinish ) );
+		fillAmountAnimateCoroutine = StartCoroutine( fillAmountAnimate( toValue, duration, optionalMarker, onFinish ) );
 	}
 
-	IEnumerator fillAmountAnimate( float toValue, float duration, System.Action onFinish = null  )
+	IEnumerator fillAmountAnimate( float toValue, float duration, RectTransform optionalMarker = null, System.Action onFinish = null  )
 	{
+		float radialImageHorizontalLength = radialImage.GetComponent<RectTransform>().sizeDelta.x;
 		float startTime = Time.time;
 		float elapsedTime = 0;
 
@@ -29,6 +30,7 @@ public class UIAnimateRadialImage : MonoBehaviour {
 			elapsedTime = Time.time - startTime;
 
 			radialImage.fillAmount =  Mathf.Lerp( fromValue, toValue, elapsedTime/duration );
+			if( optionalMarker != null ) optionalMarker.anchoredPosition = new Vector2( radialImage.fillAmount * radialImageHorizontalLength, optionalMarker.anchoredPosition.y );
 			yield return new WaitForEndOfFrame();  
 	    }
 		if( onFinish != null ) onFinish.Invoke();
