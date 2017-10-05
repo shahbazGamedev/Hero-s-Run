424f99b57b197cf3a2474c9db8091d139b62954e
korbain <regis.g@videotron.ca>
2017-10-05 17:00:06 -0400

    Skill bonus handled by handler

--- Assets/Scripts/Multiplayer/HUDMultiplayer.cs
+++ Assets/Scripts/Multiplayer/HUDMultiplayer.cs
@@ -47,9 +47,6 @@ public class HUDMultiplayer : MonoBehaviour {
 	[Header("Health Bar")]
 	[SerializeField] HealthBarHandler healthBarHandler;
 	[SerializeField] Image damageTakenEffect;
-	[Header("Skill Bonus")]
-	[SerializeField] RectTransform skillBonusHolder;
-	[SerializeField] GameObject skillBonusPrefab;
 	[Header("Stasis Tap Instructions")]
 	[SerializeField] GameObject stasisTapInstructions;
 
@@ -301,35 +298,6 @@ public class HUDMultiplayer : MonoBehaviour {
 		if( distance <= 0.5f ) distance = 0; //Added as a safeguard.
 		distanceText.text = distance.ToString("N0") + " <color=#FF396D><size=38><sub>M</sub></size></color>";
 		distanceRemainingCounterRed.fillAmount = distance/generateLevel.levelLengthInMeters;
-
-		#if UNITY_EDITOR
-		handleKeyboard();
-		#endif
-	}
-
-	private void handleKeyboard()
-	{
-		//Also support keys for debugging
-		if ( Input.GetKeyDown (KeyCode.B) ) 
-		{
-			StartCoroutine( showSkillBonus( "Trap Activated +25 XP" ) );
-		}
-	}
-
-	public IEnumerator showSkillBonus( string bonusText )
-	{
-		//Fade in 0.8 sec,stay 2.6 sec, fade-out 0.6 sec
-		GameObject skillBonus = GameObject.Instantiate( skillBonusPrefab );
-		skillBonus.GetComponent<CanvasGroup>().alpha = 0;
-		skillBonus.GetComponent<FadeInCanvasGroup>().fadeIn();
-		RectTransform skillBonusRectTransform = skillBonus.GetComponent<RectTransform>();
-		skillBonusRectTransform.SetParent( skillBonusHolder );
-		skillBonusRectTransform.localScale = Vector3.one;
-		skillBonusRectTransform.anchoredPosition = new Vector2( 0,0 );
-		yield return new WaitForSeconds( 2.6f );
-		LeanTween.moveLocalY( skillBonus, skillBonusRectTransform.anchoredPosition.y + skillBonusRectTransform.sizeDelta.y, 1f );
-		skillBonus.GetComponent<FadeInCanvasGroup>().fadeOut();
-		Destroy( skillBonus, 1f );
 	}
 
 	#region Stasis Tap Instructions
