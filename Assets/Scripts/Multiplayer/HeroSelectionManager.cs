b535280811d7bc05116113acacc02e5ae42056e9
korbain <regis.g@videotron.ca>
2017-11-14 12:39:03 -0500

    Fixed incorrect VO lines being loaded in hero selection

--- Assets/Scripts/Multiplayer/HeroSelectionManager.cs
+++ Assets/Scripts/Multiplayer/HeroSelectionManager.cs
@@ -8,13 +8,13 @@ using UnityEngine.EventSystems;
 public class HeroSelectionManager : MonoBehaviour, IPointerDownHandler {
 
 	[Header("Hero Selection Manager")]
-	[SerializeField] GameObject voiceLinesPanel;
+	[SerializeField] GameObject voiceLinesScrollView;
 	bool levelLoading = false;
 
 	// Use this for initialization
 	void Start ()
 	{
-		voiceLinesPanel.SetActive( false );
+		voiceLinesScrollView.SetActive( false );
 		Handheld.StopActivityIndicator();
 	}
 
@@ -28,13 +28,13 @@ public class HeroSelectionManager : MonoBehaviour, IPointerDownHandler {
 	public void OnClickDisplayVoiceLines()
 	{
 		UISoundManager.uiSoundManager.playButtonClick();
-		voiceLinesPanel.SetActive( !voiceLinesPanel.activeSelf );
+		voiceLinesScrollView.SetActive( !voiceLinesScrollView.activeSelf );
 	}
 
 	public void OnPointerDown(PointerEventData eventData )
 	{
 		//If the player clicks on the voice line panel outside of the button area, dismiss the panel.
-		if( voiceLinesPanel.activeSelf ) voiceLinesPanel.SetActive( false );
+		if( voiceLinesScrollView.activeSelf ) voiceLinesScrollView.SetActive( false );
 	}
 
 	IEnumerator loadScene(GameScenes value)
