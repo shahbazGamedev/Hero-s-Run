a73ed468c2b5f7a728ffd569ba5644b6d695db93
korbain <regis.g@videotron.ca>
2017-11-16 12:46:11 -0500

    removed unneeded code in object sound

--- Assets/Scripts/Audio/ObjectSound.cs
+++ Assets/Scripts/Audio/ObjectSound.cs
@@ -12,7 +12,7 @@ public class ObjectSound : MonoBehaviour {
 	{
 		//Debug.Log("ObjectSound: Name: " + name + " Root: " + transform.root.name + " Relative Velocity Sqr Magnitude: " + collision.relativeVelocity.sqrMagnitude );
 		float impactStrength = collision.relativeVelocity.sqrMagnitude;
-		if( !GetComponent<AudioSource>().isPlaying && impactStrength > 0 )
+		if( impactStrength > 0 )
 		{
 			//The collision.relativeVelocity.sqrMagnitude ranges between 0 and 300 or so.
 			//The higher the impact strength, the louder the sound.
