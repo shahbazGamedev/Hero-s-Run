73287a721ccfb0a986093456aa8b325c84a7dffa
korbain <regis.g@videotron.ca>
2017-12-16 14:37:28 -0500

    New trigger to activate a virtual camera and pause players

---
+++ Assets/Scripts/Level/Coop/TriggerVirtualCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using Cinemachine;
+
+public class TriggerVirtualCamera : MonoBehaviour {
+
+	[SerializeField] Transform lookAtCamera;
+	[SerializeField] CinemachineVirtualCamera cmvc;
+	[SerializeField] float duration = 2.5f;
+
+	bool hasBeenActivated = false;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if( other.CompareTag("Player") && !hasBeenActivated )
+		{
+			hasBeenActivated = true;
+			if( lookAtCamera != null ) cmvc.m_LookAt = lookAtCamera;
+			cmvc.enabled = true;
+			Invoke( "deactivateCamera", duration );
+			for( int i = 0; i < PlayerRace.players.Count; i++ )
+			{
+				PlayerRace.players[i].GetComponent<PlayerControl>().pausePlayer( true );
+			}
+		}
+	}
+
+	void deactivateCamera()
+	{
+		cmvc.enabled = false;
+		for( int i = 0; i < PlayerRace.players.Count; i++ )
+		{
+			PlayerRace.players[i].GetComponent<PlayerControl>().pausePlayer( false );
+		}
+	}
+}
