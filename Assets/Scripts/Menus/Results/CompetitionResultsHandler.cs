94bf1e898a61d27e163c443b8f928fca92a57f40
korbain <regis.g@videotron.ca>
2018-02-12 12:25:29 -0500

    Nicer looking solo results

--- Assets/Scripts/Menus/Results/CompetitionResultsHandler.cs
+++ Assets/Scripts/Menus/Results/CompetitionResultsHandler.cs
@@ -19,28 +19,62 @@ public class CompetitionResultsHandler : ResultsHandler {
 	[SerializeField] GameObject versusText;
 
 	[Header("Player")]
+	[SerializeField] GameObject ribbonPlayer;
 	[SerializeField] GameObject winnerPlayer;
 	[SerializeField] Image iconPlayer;
 	[SerializeField] TextMeshProUGUI namePlayer;
 	[SerializeField] GameObject emoteGameObjectPlayer;
 	[SerializeField] TextMeshProUGUI cpPlayer;
 
+	[Header("Player Solo")]
+	[SerializeField] GameObject ribbonPlayerSolo;
+	[SerializeField] Image iconPlayerSolo;
+	[SerializeField] TextMeshProUGUI namePlayerSolo;
+
 	[Header("Reward Text")]
 	[SerializeField] GameObject rewardText;
 
+	private void showResultsSolo( PlayerRace localPlayerRace )
+	{
+		#region Opponent area
+		winnerOpponent.SetActive( false );
+		ribbonOpponent.SetActive( false ); //also hides the name, and the icon.
+		#endregion
+
+		versusText.SetActive( false );
+
+		#region Player area
+		winnerPlayer.SetActive( false );
+		ribbonPlayer.SetActive( false ); //also hides the name, and the icon.
+		#endregion
+
+		#region Player Solo area
+		ribbonPlayerSolo.SetActive( true );
+		PlayerMatchData pmd = LevelManager.Instance.getPlayerMatchDataByName( localPlayerRace.name );
+		Sprite playerIconSprite = ProgressionManager.Instance.getPlayerIconSpriteByUniqueId( pmd.playerIcon ).icon;
+		iconPlayerSolo.sprite = playerIconSprite;
+		namePlayerSolo.text = localPlayerRace.name;
+		#endregion
+
+		#region Reward area
+		//You don't get any rewards when playing solo.
+		rewardText.SetActive( false );
+		#endregion
+	}
+
 	public void showResults( PlayerRace localPlayerRace )
 	{
-		//From top to bottom
-		PlayerMatchData pmd;
-		#region Opponent area
 		if( GameManager.Instance.getPlayMode() == PlayMode.PlayAlone )
 		{
-			winnerOpponent.SetActive( false );
-			ribbonOpponent.SetActive( false ); //also hides the name, and the icon.
-			versusText.SetActive( false );
+			showResultsSolo( localPlayerRace );
 		}
 		else
 		{
+			ribbonPlayerSolo.SetActive( false );
+
+			//From top to bottom
+			PlayerMatchData pmd;
+			#region Opponent area
 			PlayerRace opponent = getOtherPlayer( localPlayerRace );
 			pmd = LevelManager.Instance.getPlayerMatchDataByName( opponent.name );
 			winnerOpponent.gameObject.SetActive( opponent.racePosition == RacePosition.FIRST_PLACE ) ;
@@ -49,76 +83,43 @@ public class CompetitionResultsHandler : ResultsHandler {
 			nameOpponent.text = opponent.name;
 			emoteGameObjectOpponent.name = opponent.name;
 			emotesList.Add( emoteGameObjectOpponent );
-		}
-		#endregion
-
-		#region Player area
-		pmd = LevelManager.Instance.getPlayerMatchDataByName( localPlayerRace.name );
-		winnerPlayer.SetActive( localPlayerRace.racePosition == RacePosition.FIRST_PLACE ) ;
-		Sprite playerIconSprite = ProgressionManager.Instance.getPlayerIconSpriteByUniqueId( pmd.playerIcon ).icon;
-		iconPlayer.sprite = playerIconSprite;
-		namePlayer.text = localPlayerRace.name;
-		int competitivePointsEarnedLastRace = GameManager.Instance.playerProfile.getCompetitivePointsEarnedLastRace();
-		if( competitivePointsEarnedLastRace > 0 )
-		{
-			cpPlayer.text = "+" + competitivePointsEarnedLastRace.ToString();
-		}
-		else
-		{
-			cpPlayer.text = competitivePointsEarnedLastRace.ToString();
-		}
-		emoteGameObjectPlayer.name = localPlayerRace.name;
-		emotesList.Add( emoteGameObjectPlayer );
-		#endregion
-
-		#region Reward boxes
-		if( GameManager.Instance.getPlayMode() == PlayMode.PlayAlone )
-		{
-			//You don't get any rewards when playing alone.
-			rewardText.SetActive( false );
-		}
-		else if( GameManager.Instance.getPlayMode() == PlayMode.PlayAgainstOneFriend )
-		{
-			//You don't get a loot box when playing with a friend, even if you won.
+			#endregion
 	
-			//You don't get soft currency when playing with a friend, even if you won.
-	
-			//XP
-			//You earn XP when playing with a friend, regardless of whether you won or lost.
-			displayXP();	  		
-
-			//Save the rewards
-			saveRewards();	
-		}
-		else if( GameManager.Instance.getPlayMode() == PlayMode.PlayAgainstOnePlayer )
-		{
-			//Loot Box.
-			//You only get a loot box if you won.
-			if( localPlayerRace.racePosition == RacePosition.FIRST_PLACE ) displayLootBox();
-	
-			//Soft Currency.
-			//You only win soft currency if you won. The amount is determined by the player's current sector.
-			if( localPlayerRace.racePosition == RacePosition.FIRST_PLACE )
+			#region Player area
+			pmd = LevelManager.Instance.getPlayerMatchDataByName( localPlayerRace.name );
+			winnerPlayer.SetActive( localPlayerRace.racePosition == RacePosition.FIRST_PLACE ) ;
+			Sprite playerIconSprite = ProgressionManager.Instance.getPlayerIconSpriteByUniqueId( pmd.playerIcon ).icon;
+			iconPlayer.sprite = playerIconSprite;
+			namePlayer.text = localPlayerRace.name;
+			int competitivePointsEarnedLastRace = GameManager.Instance.playerProfile.getCompetitivePointsEarnedLastRace();
+			if( competitivePointsEarnedLastRace > 0 )
 			{
-				int currentSector = GameManager.Instance.playerProfile.getCurrentSector();
-				int softCurrencyGranted = SectorManager.Instance.getSectorVictorySoftCurrency( currentSector );
-				displaySoftCurrency( softCurrencyGranted );
+				cpPlayer.text = "+" + competitivePointsEarnedLastRace.ToString();
 			}
-	
-			//XP
-			//You earn XP regardless of whether you won or lost.
-			displayXP();	  		
-
-			//Save the rewards
-			saveRewards();	
-		}
-		else if( GameManager.Instance.getPlayMode() == PlayMode.PlayAgainstOneBot )
-		{
-			if( Debug.isDebugBuild )
+			else
 			{
-				//You normally only earn XP when playing against a bot.
-				//However, to facilitate testing, we grant the same rewards as PlayAgainstOnePlayer if this is a Debug build.
-
+				cpPlayer.text = competitivePointsEarnedLastRace.ToString();
+			}
+			emoteGameObjectPlayer.name = localPlayerRace.name;
+			emotesList.Add( emoteGameObjectPlayer );
+			#endregion
+	
+			#region Reward boxes
+			if( GameManager.Instance.getPlayMode() == PlayMode.PlayAgainstOneFriend )
+			{
+				//You don't get a loot box when playing with a friend, even if you won.
+		
+				//You don't get soft currency when playing with a friend, even if you won.
+		
+				//XP
+				//You earn XP when playing with a friend, regardless of whether you won or lost.
+				displayXP();	  		
+	
+				//Save the rewards
+				saveRewards();	
+			}
+			else if( GameManager.Instance.getPlayMode() == PlayMode.PlayAgainstOnePlayer )
+			{
 				//Loot Box.
 				//You only get a loot box if you won.
 				if( localPlayerRace.racePosition == RacePosition.FIRST_PLACE ) displayLootBox();
@@ -133,23 +134,51 @@ public class CompetitionResultsHandler : ResultsHandler {
 				}
 		
 				//XP
-				//You always earn XP regardless of whether you won or lost.
+				//You earn XP regardless of whether you won or lost.
 				displayXP();	  		
+	
+				//Save the rewards
+				saveRewards();	
 			}
-			else
+			else if( GameManager.Instance.getPlayMode() == PlayMode.PlayAgainstOneBot )
 			{
-				//You don't get a loot box when playing against a bot, even if you won.
-		
-				//You don't get soft currency when playing against a bot, even if you won.
-		
-				//XP
-				//You can earn XP when playing against a bot, regardless of whether you won or lost.
-				displayXP();	  		
+				if( Debug.isDebugBuild )
+				{
+					//You normally only earn XP when playing against a bot.
+					//However, to facilitate testing, we grant the same rewards as PlayAgainstOnePlayer if this is a Debug build.
+	
+					//Loot Box.
+					//You only get a loot box if you won.
+					if( localPlayerRace.racePosition == RacePosition.FIRST_PLACE ) displayLootBox();
+			
+					//Soft Currency.
+					//You only win soft currency if you won. The amount is determined by the player's current sector.
+					if( localPlayerRace.racePosition == RacePosition.FIRST_PLACE )
+					{
+						int currentSector = GameManager.Instance.playerProfile.getCurrentSector();
+						int softCurrencyGranted = SectorManager.Instance.getSectorVictorySoftCurrency( currentSector );
+						displaySoftCurrency( softCurrencyGranted );
+					}
+			
+					//XP
+					//You always earn XP regardless of whether you won or lost.
+					displayXP();	  		
+				}
+				else
+				{
+					//You don't get a loot box when playing against a bot, even if you won.
+			
+					//You don't get soft currency when playing against a bot, even if you won.
+			
+					//XP
+					//You can earn XP when playing against a bot, regardless of whether you won or lost.
+					displayXP();	  		
+				}
+				//Save the rewards
+				saveRewards();	
 			}
-			//Save the rewards
-			saveRewards();	
+			#endregion
 		}
-		#endregion
 
 		//Okay button
 		okayButton.onClick.RemoveAllListeners();
