f00fdeb8dfacd0e3d7bf7cb6e6143fe1517ed6ca
korbain <regis.g@videotron.ca>
2018-03-03 00:37:55 -0500

    Zombie Hand Sequence in Start tile

---
+++ Assets/Level/Creatures/Zombies/ZombieHand.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ZombieHand : MonoBehaviour {
+
+	[SerializeField] float startDelay = 2f;
+	[SerializeField] ParticleSystem spurt;
+	[SerializeField] Rigidbody looseWoodPlank;
+	[SerializeField] Vector3 woodPlankForce;
+	[SerializeField] Vector3 woodPlankTorque;
+
+	void Start ()
+	{
+		StartCoroutine( zombieHandPierceUp() );
+	}
+	
+	IEnumerator zombieHandPierceUp()
+	{
+		yield return new WaitForSeconds( startDelay );
+
+		spurt.Play();
+
+		LeanTween.moveLocalY(gameObject, transform.position.y + 1.1f, 0.5f ).setEase(LeanTweenType.easeOutExpo).setOnComplete(zombieHandAnimation).setOnCompleteParam(gameObject);
+		GetComponent<AudioSource>().PlayDelayed(0.1f);
+		if( looseWoodPlank != null )
+		{
+			looseWoodPlank.AddForce( woodPlankForce );	
+			looseWoodPlank.AddTorque( woodPlankTorque );
+		}
+	}
+	
+	void zombieHandAnimation()
+	{
+		GetComponent<Animation>().Play("FistToSearch" );
+		GetComponent<Animation>().PlayQueued("Search", QueueMode.CompleteOthers );
+	}
+}
