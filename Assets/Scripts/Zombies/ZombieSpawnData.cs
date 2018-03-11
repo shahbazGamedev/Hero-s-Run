fb5fa45f266c7f8f1b25d2f715967f8432c783d2
korbain <regis.g@videotron.ca>
2018-03-11 17:53:00 -0400

    Added tooltips

--- Assets/Scripts/Zombies/ZombieSpawnData.cs
+++ Assets/Scripts/Zombies/ZombieSpawnData.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public enum ZombieSpawnType
 {
-
 	BurrowUp = 1,
 	StandUpFromBack = 2,
 	Walk = 3,
@@ -14,11 +13,11 @@ public enum ZombieSpawnType
 
 public class ZombieSpawnData : MonoBehaviour {
 
-	//Delay before zombie is spawned
+	[Tooltip("Delay before the zombie is spawned. This is usefull so that zombies don't start their animation at the same time.")]
 	public float spawnDelay = 0;
 	public ZombieSpawnType spawnType = ZombieSpawnType.BurrowUp;
-	//If a coffin is specified, the coffin open animation will be played at the same time as the zombie is spawned
+	[Tooltip("If a coffin is specified, the coffin open animation will be played at the same time as the zombie is spawned.")]
 	public bool addCoffin;
-	//If true, the zombie heads for the player (as opposed to staying in its lane).
+	[Tooltip("If true, the zombie heads for the player (as opposed to staying in its lane).")]
 	public bool followsPlayer = false;
 }
